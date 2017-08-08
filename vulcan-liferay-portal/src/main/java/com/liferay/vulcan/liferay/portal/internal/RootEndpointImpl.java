5345f899b33d868dcf2b91ba68b613bef2b8065f
Alejandro Hernández <info@alejandrohdezma.com>
2017-08-08 10:49:21 +0200

    VULCAN-9 getRoutes should return an optional value

--- vulcan-liferay-portal/src/main/java/com/liferay/vulcan/liferay/portal/internal/RootEndpointImpl.java
+++ vulcan-liferay-portal/src/main/java/com/liferay/vulcan/liferay/portal/internal/RootEndpointImpl.java
@@ -16,10 +16,14 @@ package com.liferay.vulcan.liferay.portal.internal;
 
 import com.liferay.vulcan.endpoint.RootEndpoint;
 import com.liferay.vulcan.resource.Routes;
+import com.liferay.vulcan.result.Try;
 import com.liferay.vulcan.wiring.osgi.manager.ResourceManager;
+
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Context;
 
 import org.osgi.service.component.annotations.Component;
@@ -34,8 +38,12 @@ import org.osgi.service.component.annotations.Reference;
 public class RootEndpointImpl implements RootEndpoint {
 
 	@Override
-	public <T> Routes<T> getRoutes(String path) {
-		return _resourceManager.getRoutes(path, _httpServletRequest);
+	public <T> Try<Routes<T>> getRoutes(String path) {
+		Optional<Routes<T>> optional = _resourceManager.getRoutes(
+			path, _httpServletRequest);
+
+		return Try.fromFallible(
+			() -> optional.orElseThrow(NotFoundException::new));
 	}
 
 	@Context
