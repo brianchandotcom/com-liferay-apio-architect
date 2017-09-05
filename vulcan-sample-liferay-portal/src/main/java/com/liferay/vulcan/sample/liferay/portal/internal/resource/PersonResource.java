4632e4972b15ed7a312445d74f8b21a9330ca164
Alejandro Hernández <info@alejandrohdezma.com>
2017-09-05 15:46:11 +0200

    VULCAN-78 Add support for POST operation to PersonResource

--- vulcan-sample-liferay-portal/src/main/java/com/liferay/vulcan/sample/liferay/portal/internal/resource/PersonResource.java
+++ vulcan-sample-liferay-portal/src/main/java/com/liferay/vulcan/sample/liferay/portal/internal/resource/PersonResource.java
@@ -18,9 +18,14 @@ import com.liferay.portal.kernel.exception.NoSuchUserException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.UserConstants;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
-import com.liferay.portal.kernel.service.UserService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.DateUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.vulcan.identifier.LongIdentifier;
 import com.liferay.vulcan.identifier.RootIdentifier;
 import com.liferay.vulcan.pagination.PageItems;
@@ -29,12 +34,20 @@ import com.liferay.vulcan.resource.Resource;
 import com.liferay.vulcan.resource.Routes;
 import com.liferay.vulcan.resource.builder.RepresentorBuilder;
 import com.liferay.vulcan.resource.builder.RoutesBuilder;
+import com.liferay.vulcan.result.Try;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.ServerErrorException;
 
@@ -59,14 +72,14 @@ public class PersonResource implements Resource<User, LongIdentifier> {
 		RepresentorBuilder<User, LongIdentifier> representorBuilder) {
 
 		Function<User, Object> birthDateFunction = user -> {
-			try {
-				DateFormat dateFormat = DateUtil.getISO8601Format();
+			Try<DateFormat> dateFormatTry = Try.success(
+				DateUtil.getISO8601Format());
 
-				return dateFormat.format(user.getBirthday());
-			}
-			catch (PortalException pe) {
-				throw new ServerErrorException(500, pe);
-			}
+			return dateFormatTry.map(
+				dateFormat -> dateFormat.format(user.getBirthday())
+			).orElseThrow(
+				() -> new ServerErrorException(500)
+			);
 		};
 
 		representorBuilder.identifier(
@@ -74,11 +87,24 @@ public class PersonResource implements Resource<User, LongIdentifier> {
 		).addField(
 			"additionalName", User::getMiddleName
 		).addField(
+			"alternateName", User::getScreenName
+		).addField(
 			"birthDate", birthDateFunction
 		).addField(
 			"email", User::getEmailAddress
 		).addField(
 			"familyName", User::getLastName
+		).addField(
+			"gender",
+			user -> {
+				Try<Boolean> booleanTry = Try.fromFallible(user::isMale);
+
+				return booleanTry.map(
+					male -> male ? "male" : "female"
+				).orElse(
+					null
+				);
+			}
 		).addField(
 			"givenName", User::getFirstName
 		).addField(
@@ -102,29 +128,95 @@ public class PersonResource implements Resource<User, LongIdentifier> {
 			this::_getUser
 		).collectionPage(
 			this::_getPageItems, RootIdentifier.class, Company.class
+		).postCollectionItem(
+			this::_addUser, RootIdentifier.class, Company.class
 		).build();
+	}
+
+	private User _addUser(
+		RootIdentifier rootIdentifier, Map<String, Object> body,
+		Company company) {
+
+		String screenName = (String)body.get("alternateName");
+		String emailAddress = (String)body.get("email");
+		String firstName = (String)body.get("givenName");
+		String lastName = (String)body.get("familyName");
+		String jobTitle = (String)body.get("jobTitle");
+		String birthDateString = (String)body.get("birthDate");
+		String password1 = (String)body.get("password1");
+		String password2 = (String)body.get("password2");
+		boolean male = (boolean)body.get("male");
+
+		Supplier<BadRequestException> incorrectBodyExceptionSupplier =
+			() -> new BadRequestException("Incorrect body");
+
+		if (Validator.isNull(screenName) || Validator.isNull(emailAddress) ||
+			Validator.isNull(firstName) || Validator.isNull(lastName) ||
+			Validator.isNull(jobTitle) || Validator.isNull(birthDateString)) {
+
+			throw incorrectBodyExceptionSupplier.get();
+		}
+
+		boolean autoPassword = false;
+		boolean autoScreenName = Validator.isNull(screenName);
+		long facebookId = 0;
+		String openId = StringPool.BLANK;
+		Locale locale = LocaleUtil.getDefault();
+		String middleName = StringPool.BLANK;
+		long prefixId = 0;
+		long suffixId = 0;
+
+		Try<DateFormat> dateFormatTry = Try.success(
+			DateUtil.getISO8601Format());
+
+		Date birthDate = dateFormatTry.map(
+			dateFormat -> dateFormat.parse(birthDateString)
+		).mapFailMatching(
+			ParseException.class, incorrectBodyExceptionSupplier
+		).getUnchecked();
+
+		Calendar calendar = Calendar.getInstance();
+
+		calendar.setTime(birthDate);
+
+		int birthdayMonth = calendar.get(Calendar.MONTH);
+		int birthdayDay = calendar.get(Calendar.DATE);
+		int birthdayYear = calendar.get(Calendar.YEAR);
+
+		long[] groupIds = null;
+		long[] organizationIds = null;
+		long[] roleIds = null;
+		long[] userGroupIds = null;
+		boolean sendMail = false;
+
+		Try<User> userTry = Try.fromFallible(
+			() -> _userLocalService.addUser(
+				UserConstants.USER_ID_DEFAULT, company.getCompanyId(),
+				autoPassword, password1, password2, autoScreenName, screenName,
+				emailAddress, facebookId, openId, locale, firstName, middleName,
+				lastName, prefixId, suffixId, male, birthdayMonth, birthdayDay,
+				birthdayYear, jobTitle, groupIds, organizationIds, roleIds,
+				userGroupIds, sendMail, new ServiceContext()));
+
+		return userTry.getUnchecked();
 	}
 
 	private PageItems<User> _getPageItems(
 		Pagination pagination, RootIdentifier rootIdentifier, Company company) {
 
-		try {
-			List<User> users = _userService.getCompanyUsers(
-				company.getCompanyId(), pagination.getStartPosition(),
-				pagination.getEndPosition());
-			int count = _userService.getCompanyUsersCount(
-				company.getCompanyId());
+		List<User> users = _userLocalService.getCompanyUsers(
+			company.getCompanyId(), pagination.getStartPosition(),
+			pagination.getEndPosition());
+		int count = _userLocalService.getCompanyUsersCount(
+			company.getCompanyId());
 
-			return new PageItems<>(users, count);
-		}
-		catch (PortalException pe) {
-			throw new ServerErrorException(500, pe);
-		}
+		return new PageItems<>(users, count);
 	}
 
 	private User _getUser(LongIdentifier userLongIdentifier) {
 		try {
-			return _userService.getUserById(userLongIdentifier.getIdAsLong());
+			return _userLocalService.getUserById(
+				userLongIdentifier.getIdAsLong());
 		}
 		catch (NoSuchUserException | PrincipalException e) {
 			throw new NotFoundException(
@@ -136,6 +228,6 @@ public class PersonResource implements Resource<User, LongIdentifier> {
 	}
 
 	@Reference
-	private UserService _userService;
+	private UserLocalService _userLocalService;
 
 }
