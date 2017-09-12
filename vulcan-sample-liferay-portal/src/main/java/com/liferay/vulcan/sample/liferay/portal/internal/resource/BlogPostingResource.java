3b75a59775aa8916cfac4217ce78e7e714e58552
Brian Chan <brian.chan@liferay.com>
2017-09-11 21:21:01 -0700

    VULCAN-78 I don't mind skipping var declaration, but you're just not being consistent with your other *Resource.java. If you care, feel free to specify the null vars, or change them in your other *Resource. For now, I'm going to revert this.

--- vulcan-sample-liferay-portal/src/main/java/com/liferay/vulcan/sample/liferay/portal/internal/resource/BlogPostingResource.java
+++ vulcan-sample-liferay-portal/src/main/java/com/liferay/vulcan/sample/liferay/portal/internal/resource/BlogPostingResource.java
@@ -190,6 +190,9 @@ public class BlogPostingResource
 		int hour = calendar.get(Calendar.HOUR);
 		int minute = calendar.get(Calendar.MINUTE);
 
+		boolean allowPingbacks = false;
+		boolean allowTrackbacks = false;
+
 		ServiceContext serviceContext = new ServiceContext();
 
 		serviceContext.setAddGroupPermissions(true);
@@ -199,7 +202,8 @@ public class BlogPostingResource
 		Try<BlogsEntry> blogsEntryTry = Try.fromFallible(
 			() -> _blogsService.addEntry(
 				title, subtitle, description, content, month, day, year, hour,
-				minute, false, false, null, null, null, null, serviceContext));
+				minute, allowPingbacks, allowTrackbacks, null, null, null, null,
+				serviceContext));
 
 		return blogsEntryTry.getUnchecked();
 	}
