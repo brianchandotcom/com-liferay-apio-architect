a6ad5430a09caa413f8deba74136523a2ad10761
Alejandro Hernández <info@alejandrohdezma.com>
2017-10-18 12:02:32 +0200

    VULCAN-88 Recover sample resources following Brian's suggestion

---
+++ vulcan-sample/src/main/java/com/liferay/vulcan/sample/internal/resource/BlogPostingCollectionResource.java
@@ -0,0 +1,158 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.vulcan.sample.internal.resource;
+
+import com.liferay.vulcan.pagination.PageItems;
+import com.liferay.vulcan.pagination.Pagination;
+import com.liferay.vulcan.resource.CollectionResource;
+import com.liferay.vulcan.resource.Representor;
+import com.liferay.vulcan.resource.Routes;
+import com.liferay.vulcan.resource.builder.RepresentorBuilder;
+import com.liferay.vulcan.resource.builder.RoutesBuilder;
+import com.liferay.vulcan.resource.identifier.LongIdentifier;
+import com.liferay.vulcan.resource.identifier.RootIdentifier;
+import com.liferay.vulcan.sample.internal.model.BlogPosting;
+import com.liferay.vulcan.sample.internal.model.BlogPostingComment;
+import com.liferay.vulcan.sample.internal.model.Person;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.ws.rs.NotFoundException;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Provides all the necessary information to expose <a
+ * href="http://schema.org/BlogPosting">BlogPosting</a> resources through a web
+ * API.
+ *
+ * The resources are mapped from the internal {@link BlogPosting} model.
+ *
+ * @author Alejandro Hernández
+ * @review
+ */
+@Component(immediate = true)
+public class BlogPostingCollectionResource
+	implements CollectionResource<BlogPosting, LongIdentifier> {
+
+	@Override
+	public Representor<BlogPosting, LongIdentifier> buildRepresentor(
+		RepresentorBuilder<BlogPosting, LongIdentifier> representorBuilder) {
+
+		return representorBuilder.identifier(
+			blogPosting -> blogPosting::getId
+		).addDate(
+			"dateCreated", BlogPosting::getCreateDate
+		).addDate(
+			"dateModified", BlogPosting::getModifiedDate
+		).addEmbeddedModel(
+			"creator", Person.class,
+			blogPosting -> Person.getPerson(blogPosting.getCreatorId())
+		).addRelatedCollection(
+			"comments", BlogPostingComment.class,
+			blogPosting -> (LongIdentifier)blogPosting::getId
+		).addString(
+			"alternativeHeadline", BlogPosting::getSubtitle
+		).addString(
+			"articleBody", BlogPosting::getContent
+		).addString(
+			"fileFormat", blogPosting -> "text/html"
+		).addString(
+			"headline", BlogPosting::getTitle
+		).addType(
+			"BlogPosting"
+		).build();
+	}
+
+	@Override
+	public String getName() {
+		return "blog-postings";
+	}
+
+	@Override
+	public Routes<BlogPosting> routes(
+		RoutesBuilder<BlogPosting, LongIdentifier> routesBuilder) {
+
+		return routesBuilder.addCollectionPageGetter(
+			this::_getPageItems, RootIdentifier.class
+		).addCollectionPageItemCreator(
+			this::_addBlogPosting, RootIdentifier.class
+		).addCollectionPageItemGetter(
+			this::_getBlogPosting
+		).addCollectionPageItemRemover(
+			this::_deleteBlogPosting
+		).addCollectionPageItemUpdater(
+			this::_updateBlogPosting
+		).build();
+	}
+
+	private BlogPosting _addBlogPosting(
+		RootIdentifier rootIdentifier, Map<String, Object> body) {
+
+		String content = (String)body.get("articleBody");
+		Long creatorId = (Long)body.get("creator");
+		String subtitle = (String)body.get("alternativeHeadline");
+		String title = (String)body.get("headline");
+
+		return BlogPosting.addBlogPosting(title, subtitle, content, creatorId);
+	}
+
+	private void _deleteBlogPosting(LongIdentifier blogPostingLongIdentifier) {
+		BlogPosting.deleteBlogPosting(blogPostingLongIdentifier.getId());
+	}
+
+	private BlogPosting _getBlogPosting(
+		LongIdentifier blogPostingLongIdentifier) {
+
+		Optional<BlogPosting> optional = BlogPosting.getBlogPosting(
+			blogPostingLongIdentifier.getId());
+
+		return optional.orElseThrow(
+			() -> new NotFoundException(
+				"Unable to get blog posting " +
+					blogPostingLongIdentifier.getId()));
+	}
+
+	private PageItems<BlogPosting> _getPageItems(
+		Pagination pagination, RootIdentifier rootIdentifier) {
+
+		List<BlogPosting> blogPostings = BlogPosting.getBlogPostings(
+			pagination.getStartPosition(), pagination.getEndPosition());
+		int count = BlogPosting.getBlogPostingCount();
+
+		return new PageItems<>(blogPostings, count);
+	}
+
+	private BlogPosting _updateBlogPosting(
+		LongIdentifier blogPostingLongIdentifier, Map<String, Object> body) {
+
+		String content = (String)body.get("articleBody");
+		Long creatorId = (Long)body.get("creator");
+		String subtitle = (String)body.get("alternativeHeadline");
+		String title = (String)body.get("headline");
+
+		Optional<BlogPosting> optional = BlogPosting.updateBlogPosting(
+			blogPostingLongIdentifier.getId(), title, subtitle, content,
+			creatorId);
+
+		return optional.orElseThrow(
+			() -> new NotFoundException(
+				"Unable to get blog posting " +
+					blogPostingLongIdentifier.getId()));
+	}
+
+}
---
+++ vulcan-sample/src/main/java/com/liferay/vulcan/sample/internal/resource/BlogPostingCommentScopedCollectionResource.java
@@ -0,0 +1,154 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.vulcan.sample.internal.resource;
+
+import com.liferay.vulcan.pagination.PageItems;
+import com.liferay.vulcan.pagination.Pagination;
+import com.liferay.vulcan.resource.CollectionResource;
+import com.liferay.vulcan.resource.Representor;
+import com.liferay.vulcan.resource.Routes;
+import com.liferay.vulcan.resource.ScopedCollectionResource;
+import com.liferay.vulcan.resource.builder.RepresentorBuilder;
+import com.liferay.vulcan.resource.builder.RoutesBuilder;
+import com.liferay.vulcan.resource.identifier.LongIdentifier;
+import com.liferay.vulcan.sample.internal.model.BlogPostingComment;
+import com.liferay.vulcan.sample.internal.model.Person;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.ws.rs.NotFoundException;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Provides all the necessary information to expose <a
+ * href="http://schema.org/Comment">Comment</a> resources through a web API.
+ *
+ * The resources are mapped from the internal {@link BlogPostingComment} model.
+ *
+ * @author Alejandro Hernández
+ * @review
+ */
+@Component(immediate = true, service = CollectionResource.class)
+public class BlogPostingCommentScopedCollectionResource
+	implements ScopedCollectionResource<BlogPostingComment, LongIdentifier> {
+
+	@Override
+	public Representor<BlogPostingComment, LongIdentifier> buildRepresentor(
+		RepresentorBuilder<BlogPostingComment, LongIdentifier>
+			representorBuilder) {
+
+		return representorBuilder.identifier(
+			blogPostingComment -> blogPostingComment::getId
+		).addDate(
+			"dateCreated", BlogPostingComment::getCreateDate
+		).addDate(
+			"dateModified", BlogPostingComment::getModifiedDate
+		).addEmbeddedModel(
+			"author", Person.class,
+			blogPostingComment ->
+				Person.getPerson(blogPostingComment.getAuthorId())
+		).addString(
+			"text", BlogPostingComment::getContent
+		).addType(
+			"Comment"
+		).build();
+	}
+
+	@Override
+	public String getName() {
+		return "comments";
+	}
+
+	@Override
+	public Routes<BlogPostingComment> routes(
+		RoutesBuilder<BlogPostingComment, LongIdentifier> routesBuilder) {
+
+		return routesBuilder.addCollectionPageGetter(
+			this::_getPageItems, LongIdentifier.class
+		).addCollectionPageItemCreator(
+			this::_addBlogPostingComment, LongIdentifier.class
+		).addCollectionPageItemGetter(
+			this::_getBlogPostingComment
+		).addCollectionPageItemRemover(
+			this::_deleteBlogPostingComment
+		).addCollectionPageItemUpdater(
+			this::_updateBlogPostingComment
+		).build();
+	}
+
+	private BlogPostingComment _addBlogPostingComment(
+		LongIdentifier blogPostLongIdentifier, Map<String, Object> body) {
+
+		Long authorId = (Long)body.get("author");
+		String content = (String)body.get("text");
+
+		return BlogPostingComment.addBlogPostingComment(
+			blogPostLongIdentifier.getId(), authorId, content);
+	}
+
+	private void _deleteBlogPostingComment(
+		LongIdentifier blogPostingCommentLongIdentifier) {
+
+		BlogPostingComment.deleteBlogPostingComment(
+			blogPostingCommentLongIdentifier.getId());
+	}
+
+	private BlogPostingComment _getBlogPostingComment(
+		LongIdentifier blogPostingCommentLongIdentifier) {
+
+		Optional<BlogPostingComment> optional =
+			BlogPostingComment.getBlogPostingComment(
+				blogPostingCommentLongIdentifier.getId());
+
+		return optional.orElseThrow(
+			() -> new NotFoundException(
+				"Unable to get blog posting comment " +
+					blogPostingCommentLongIdentifier.getId()));
+	}
+
+	private PageItems<BlogPostingComment> _getPageItems(
+		Pagination pagination, LongIdentifier blogPostLongIdentifier) {
+
+		List<BlogPostingComment> blogsEntries =
+			BlogPostingComment.getBlogPostingComments(
+				blogPostLongIdentifier.getId(), pagination.getStartPosition(),
+				pagination.getEndPosition());
+		int count = BlogPostingComment.getBlogPostingCommentsCount(
+			blogPostLongIdentifier.getId());
+
+		return new PageItems<>(blogsEntries, count);
+	}
+
+	private BlogPostingComment _updateBlogPostingComment(
+		LongIdentifier blogPostingCommentLongIdentifier,
+		Map<String, Object> body) {
+
+		Long authorId = (Long)body.get("author");
+		String content = (String)body.get("text");
+
+		Optional<BlogPostingComment> optional =
+			BlogPostingComment.updateBlogPostingComment(
+				blogPostingCommentLongIdentifier.getId(), authorId, content);
+
+		return optional.orElseThrow(
+			() -> new NotFoundException(
+				"Unable to get blog posting comment " +
+					blogPostingCommentLongIdentifier.getId()));
+	}
+
+}
