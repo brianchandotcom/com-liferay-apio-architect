2b3c8a81078f2f36ccc2c5515484bf3e40a7ecbf
Alejandro Hernández <info@alejandrohdezma.com>
2017-11-06 12:44:40 +0100

    VULCAN-10 Write PlainJSONSingleModelMessageMapperTest using vulcan-test

--- vulcan-message-json-plain/src/test/java/com/liferay/vulcan/message/json/plain/internal/PlainJSONSingleModelMessageMapperTest.java
+++ vulcan-message-json-plain/src/test/java/com/liferay/vulcan/message/json/plain/internal/PlainJSONSingleModelMessageMapperTest.java
@@ -14,93 +14,131 @@
 
 package com.liferay.vulcan.message.json.plain.internal;
 
+import static com.liferay.vulcan.test.json.JsonMatchers.aJsonBoolean;
+import static com.liferay.vulcan.test.json.JsonMatchers.aJsonInt;
+import static com.liferay.vulcan.test.json.JsonMatchers.aJsonObjectWith;
+import static com.liferay.vulcan.test.json.JsonMatchers.aJsonString;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import com.liferay.vulcan.jaxrs.json.internal.JSONObjectBuilderImpl;
-import com.liferay.vulcan.list.FunctionalList;
+import com.liferay.vulcan.test.json.Conditions;
+import com.liferay.vulcan.test.message.MockModel;
+import com.liferay.vulcan.test.message.MockSingleModelWriter;
 
-import org.junit.Assert;
+import javax.ws.rs.core.HttpHeaders;
+
+import org.hamcrest.Matcher;
+
 import org.junit.Test;
+
+import org.mockito.Mockito;
 
 /**
  * @author Javier Gamarra
+ * @author Alejandro Hernández
  */
 public class PlainJSONSingleModelMessageMapperTest {
 
 	@Test
-	public void testMapBooleanField() {
-		_plainJSONSingleModelMessageMapper.mapBooleanField(
-			_jsonObjectBuilder, "fieldName", true);
+	public void testMediaTypeIsCorrect() {
+		String mediaType = _singleModelMessageMapper.getMediaType();
 
-		JsonObject jsonObject = _jsonObjectBuilder.build();
-
-		Assert.assertEquals("{\"fieldName\":true}", jsonObject.toString());
+		assertThat(mediaType, is(equalTo("application/json")));
 	}
 
 	@Test
-	public void testMapEmbeddedResourceBooleanField() {
-		_plainJSONSingleModelMessageMapper.mapEmbeddedResourceBooleanField(
-			_jsonObjectBuilder, new FunctionalList<>(null, "element"),
-			"fieldName", true);
+	public void testPlainJSONSingleModelMessageMapper() {
+		HttpHeaders httpHeaders = Mockito.mock(HttpHeaders.class);
 
-		JsonObject jsonObject = _jsonObjectBuilder.build();
+		JSONObjectBuilderImpl jsonObjectBuilder = new JSONObjectBuilderImpl();
 
-		Assert.assertEquals("{\"element\":true}", jsonObject.toString());
+		MockSingleModelWriter.write(
+			_singleModelMessageMapper, jsonObjectBuilder, httpHeaders);
+
+		JsonObject jsonObject = jsonObjectBuilder.build();
+
+		Conditions.Builder builder = new Conditions.Builder();
+
+		Conditions conditions = builder.where(
+			"boolean1", is(aJsonBoolean(true))
+		).where(
+			"boolean2", is(aJsonBoolean(false))
+		).where(
+			"first-linked", _isALinkTo("localhost:8080/first-linked")
+		).where(
+			"first-embedded", _isAJsonObjectWithTheFirstEmbedded
+		).where(
+			"link1", _isALinkTo("www.liferay.com")
+		).where(
+			"link2", _isALinkTo("community.liferay.com")
+		).where(
+			"number1", is(aJsonInt(equalTo(2017)))
+		).where(
+			"number2", is(aJsonInt(equalTo(42)))
+		).where(
+			"self", _isALinkTo("localhost:8080")
+		).where(
+			"string1", is(aJsonString(equalTo("Live long and prosper")))
+		).where(
+			"string2", is(aJsonString(equalTo("Hypermedia")))
+		).build();
+
+		assertThat(jsonObject, is(aJsonObjectWith(conditions)));
 	}
 
-	@Test
-	public void testMapLink() {
-		_plainJSONSingleModelMessageMapper.mapLink(
-			_jsonObjectBuilder, "fieldName", "https://localhost:8080");
-
-		JsonObject jsonObject = _jsonObjectBuilder.build();
-
-		System.out.println(jsonObject.toString());
-
-		Assert.assertEquals(
-			"{\"fieldName\":\"https://localhost:8080\"}",
-			jsonObject.toString());
+	private static Matcher<? extends JsonElement> _isALinkTo(String url) {
+		return is(aJsonString(equalTo(url)));
 	}
 
-	@Test
-	public void testMapNumberField() {
-		_plainJSONSingleModelMessageMapper.mapNumberField(
-			_jsonObjectBuilder, "fieldName", 1);
+	private static final Matcher<JsonObject> _isAJsonObjectWithTheFirstEmbedded;
 
-		JsonObject jsonObject = _jsonObjectBuilder.build();
+	static {
+		Conditions.Builder builder = new Conditions.Builder();
 
-		System.out.println(jsonObject.toString());
+		Conditions secondEmbeddedConditions = builder.where(
+			"boolean", is(aJsonBoolean(true))
+		).where(
+			"link", _isALinkTo("www.liferay.com")
+		).where(
+			"number", is(aJsonInt(equalTo(42)))
+		).where(
+			"self", _isALinkTo("localhost:8080/inner")
+		).where(
+			"string", is(aJsonString(equalTo("A string")))
+		).where(
+			"third-linked", _isALinkTo("localhost:8080/third-linked")
+		).build();
 
-		Assert.assertEquals("{\"fieldName\":1}", jsonObject.toString());
+		Matcher<JsonObject> isAJsonObjectWithTheSecondEmbedded = is(
+			aJsonObjectWith(secondEmbeddedConditions));
+
+		Conditions firstEmbeddedConditions = builder.where(
+			"boolean", is(aJsonBoolean(true))
+		).where(
+			"link", _isALinkTo("www.liferay.com")
+		).where(
+			"number", is(aJsonInt(equalTo(42)))
+		).where(
+			"second-embedded", isAJsonObjectWithTheSecondEmbedded
+		).where(
+			"second-linked", _isALinkTo("localhost:8080/second-linked")
+		).where(
+			"self", _isALinkTo("localhost:8080/inner")
+		).where(
+			"string", is(aJsonString(equalTo("A string")))
+		).build();
+
+		_isAJsonObjectWithTheFirstEmbedded = is(
+			aJsonObjectWith(firstEmbeddedConditions));
 	}
 
-	@Test
-	public void testMapSelfURL() {
-		_plainJSONSingleModelMessageMapper.mapSelfURL(
-			_jsonObjectBuilder, "https://localhost:8080");
-
-		JsonObject jsonObject = _jsonObjectBuilder.build();
-
-		System.out.println(jsonObject.toString());
-
-		Assert.assertEquals(
-			"{\"self\":\"https://localhost:8080\"}", jsonObject.toString());
-	}
-
-	@Test
-	public void testMapStringField() {
-		_plainJSONSingleModelMessageMapper.mapStringField(
-			_jsonObjectBuilder, "fieldName", "value");
-
-		JsonObject jsonObject = _jsonObjectBuilder.build();
-
-		Assert.assertEquals("{\"fieldName\":\"value\"}", jsonObject.toString());
-	}
-
-	private final JSONObjectBuilderImpl _jsonObjectBuilder =
-		new JSONObjectBuilderImpl();
-	private final PlainJSONSingleModelMessageMapper
-		_plainJSONSingleModelMessageMapper =
-			new PlainJSONSingleModelMessageMapper();
+	private final PlainJSONSingleModelMessageMapper<MockModel>
+		_singleModelMessageMapper = new PlainJSONSingleModelMessageMapper<>();
 
 }
