561a491810d3dd35f360867258a58de4e691c3c3
Alejandro Hernández <info@alejandrohdezma.com>
2017-11-16 12:17:48 +0100

    VULCAN-10 We don't need to split Representor interface and implementation

--- vulcan-api/src/main/java/com/liferay/vulcan/resource/Representor.java
+++ vulcan-api/src/main/java/com/liferay/vulcan/resource/Representor.java
@@ -14,16 +14,27 @@
 
 package com.liferay.vulcan.resource;
 
-import aQute.bnd.annotation.ProviderType;
-
 import com.liferay.vulcan.alias.BinaryFunction;
+import com.liferay.vulcan.consumer.TriConsumer;
 import com.liferay.vulcan.language.Language;
 import com.liferay.vulcan.resource.identifier.Identifier;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.TimeZone;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 /**
@@ -32,21 +43,30 @@ import java.util.stream.Stream;
  *
  * <p>
  * Instances of this interface should always be created by using a {@link
- * com.liferay.vulcan.resource.builder.RepresentorBuilder}.
+ * Representor.Builder}.
  * </p>
  *
  * @author Alejandro Hernández
- * @see    com.liferay.vulcan.resource.builder.RepresentorBuilder
+ * @see    Representor.Builder
  */
-@ProviderType
-public interface Representor<T, U extends Identifier> {
+public class Representor<T, U extends Identifier> {
+
+	public Representor(
+		Class<U> identifierClass,
+		Supplier<List<RelatedCollection<T, ?>>> relatedCollectionsSupplier) {
+
+		_identifierClass = identifierClass;
+		_relatedCollectionsSupplier = relatedCollectionsSupplier;
+	}
 
 	/**
 	 * Returns the binary resources linked to a model.
 	 *
 	 * @return the binary resources linked to a model
 	 */
-	public Map<String, BinaryFunction<T>> getBinaryFunctions();
+	public Map<String, BinaryFunction<T>> getBinaryFunctions() {
+		return _binaryFunctions;
+	}
 
 	/**
 	 * Returns the map containing the boolean field names and the functions to
@@ -54,14 +74,18 @@ public interface Representor<T, U extends Identifier> {
 	 *
 	 * @return the map containing the boolean field names and functions
 	 */
-	public Map<String, Function<T, Boolean>> getBooleanFunctions();
+	public Map<String, Function<T, Boolean>> getBooleanFunctions() {
+		return _booleanFunctions;
+	}
 
 	/**
 	 * Returns the embedded related models.
 	 *
 	 * @return the embedded related models
 	 */
-	public List<RelatedModel<T, ?>> getEmbeddedRelatedModels();
+	public List<RelatedModel<T, ?>> getEmbeddedRelatedModels() {
+		return _embeddedRelatedModels;
+	}
 
 	/**
 	 * Returns the model's identifier.
@@ -69,28 +93,36 @@ public interface Representor<T, U extends Identifier> {
 	 * @param  model the model instance
 	 * @return the model's identifier
 	 */
-	public U getIdentifier(T model);
+	public U getIdentifier(T model) {
+		return _identifierFunction.apply(model);
+	}
 
 	/**
 	 * Returns the identifier class.
 	 *
 	 * @return the identifier class
 	 */
-	public Class<U> getIdentifierClass();
+	public Class<U> getIdentifierClass() {
+		return _identifierClass;
+	}
 
 	/**
 	 * Returns the linked related models.
 	 *
 	 * @return the linked related models
 	 */
-	public List<RelatedModel<T, ?>> getLinkedRelatedModels();
+	public List<RelatedModel<T, ?>> getLinkedRelatedModels() {
+		return _linkedRelatedModels;
+	}
 
 	/**
 	 * Returns the links.
 	 *
 	 * @return the links
 	 */
-	public Map<String, String> getLinks();
+	public Map<String, String> getLinks() {
+		return _links;
+	}
 
 	/**
 	 * Returns a map containing the localized string field names and the
@@ -99,7 +131,10 @@ public interface Representor<T, U extends Identifier> {
 	 * @return the map containing the localized string field names and functions
 	 */
 	public Map<String, BiFunction<T, Language, String>>
-		getLocalizedStringFunctions();
+		getLocalizedStringFunctions() {
+
+		return _localizedStringFunctions;
+	}
 
 	/**
 	 * Returns the map containing the number field names and the functions to
@@ -107,14 +142,25 @@ public interface Representor<T, U extends Identifier> {
 	 *
 	 * @return the map containing the number field names and functions
 	 */
-	public Map<String, Function<T, Number>> getNumberFunctions();
+	public Map<String, Function<T, Number>> getNumberFunctions() {
+		return _numberFunctions;
+	}
 
 	/**
 	 * Returns the related collections.
 	 *
 	 * @return the related collections
 	 */
-	public Stream<RelatedCollection<T, ?>> getRelatedCollections();
+	public Stream<RelatedCollection<T, ?>> getRelatedCollections() {
+		Stream<List<RelatedCollection<T, ?>>> stream = Stream.of(
+			_relatedCollections, _relatedCollectionsSupplier.get());
+
+		return stream.filter(
+			Objects::nonNull
+		).flatMap(
+			Collection::stream
+		);
+	}
 
 	/**
 	 * Returns the map containing the string field names and the functions to
@@ -122,13 +168,314 @@ public interface Representor<T, U extends Identifier> {
 	 *
 	 * @return the map containing the string field names and functions
 	 */
-	public Map<String, Function<T, String>> getStringFunctions();
+	public Map<String, Function<T, String>> getStringFunctions() {
+		return _stringFunctions;
+	}
 
 	/**
 	 * Returns the types.
 	 *
 	 * @return the types
 	 */
-	public List<String> getTypes();
+	public List<String> getTypes() {
+		return _types;
+	}
+
+	/**
+	 * Creates generic representations of your domain models that Vulcan
+	 * hypermedia writers can understand.
+	 */
+	public static class Builder<T, U extends Identifier> {
+
+		public Builder(Class<U> identifierClass) {
+			Supplier<List<RelatedCollection<T, ?>>> listSupplier =
+				Collections::emptyList;
+
+			_addRelatedCollectionTriConsumer = TriConsumer.empty();
+			_representor = new Representor<>(identifierClass, listSupplier);
+		}
+
+		public Builder(
+			Class<U> identifierClass,
+			TriConsumer<String, Class<?>, Function<Object, Identifier>>
+				addRelatedCollectionTriConsumer,
+			Supplier<List<RelatedCollection<T, ?>>>
+				relatedCollectionsSupplier) {
+
+			_addRelatedCollectionTriConsumer = addRelatedCollectionTriConsumer;
+			_representor = new Representor<>(
+				identifierClass, relatedCollectionsSupplier);
+		}
+
+		/**
+		 * Provides a lambda function that can be used to obtain a model's
+		 * {@link Identifier}.
+		 *
+		 * @param  identifierFunction lambda function used to obtain a model's
+		 *         identifier
+		 * @return the builder's next step
+		 */
+		public FirstStep identifier(Function<T, U> identifierFunction) {
+			_representor._identifierFunction = identifierFunction;
+
+			return new FirstStep();
+		}
+
+		public class FirstStep {
+
+			/**
+			 * Adds information about the bidirectional relation of a linked
+			 * model in the resource and a collection of {@link
+			 * CollectionResource} items in the related resource.
+			 *
+			 * @param  key the relation's name in the resource
+			 * @param  relatedKey the relation's name in the related resource
+			 * @param  modelClass the related model's class
+			 * @param  modelFunction the function used to get the related model
+			 * @param  identifierFunction the function used to get the
+			 *         collection's identifier
+			 * @return the builder's step
+			 */
+			@SuppressWarnings("unchecked")
+			public <S> FirstStep addBidirectionalModel(
+				String key, String relatedKey, Class<S> modelClass,
+				Function<T, Optional<S>> modelFunction,
+				Function<S, Identifier> identifierFunction) {
+
+				_representor._linkedRelatedModels.add(
+					new RelatedModel<>(key, modelClass, modelFunction));
+
+				_addRelatedCollectionTriConsumer.accept(
+					relatedKey, modelClass,
+					(Function<Object, Identifier>)identifierFunction);
+
+				return this;
+			}
+
+			/**
+			 * Adds binary files to a resource.
+			 *
+			 * @param  key the binary resource's name
+			 * @param  binaryFunction the function used to get the binaries
+			 * @return the builder's step
+			 */
+			public FirstStep addBinary(
+				String key, BinaryFunction<T> binaryFunction) {
+
+				_representor._binaryFunctions.put(key, binaryFunction);
+
+				return this;
+			}
+
+			/**
+			 * Adds information about a resource's boolean field.
+			 *
+			 * @param  key the field's name
+			 * @param  booleanFunction the function used to get the boolean
+			 *         value
+			 * @return the builder's step
+			 */
+			public FirstStep addBoolean(
+				String key, Function<T, Boolean> booleanFunction) {
+
+				_representor._booleanFunctions.put(key, booleanFunction);
+
+				return this;
+			}
+
+			/**
+			 * Adds information about a resource's date field.
+			 *
+			 * @param  key the field's name
+			 * @param  dateFunction the function used to get the date value
+			 * @return the builder's step
+			 */
+			public FirstStep addDate(
+				String key, Function<T, Date> dateFunction) {
+
+				Function<Date, String> formatFunction = date -> {
+					if (date == null) {
+						return null;
+					}
+
+					TimeZone timeZone = TimeZone.getTimeZone("UTC");
+
+					DateFormat dateFormat = new SimpleDateFormat(
+						"yyyy-MM-dd'T'HH:mm'Z'");
+
+					dateFormat.setTimeZone(timeZone);
+
+					return dateFormat.format(date);
+				};
+
+				_representor._stringFunctions.put(
+					key, dateFunction.andThen(formatFunction));
+
+				return this;
+			}
+
+			/**
+			 * Adds information about an embeddable related model.
+			 *
+			 * @param  key the relation's name
+			 * @param  modelClass the related model's class
+			 * @param  modelFunction the function used to get the related model
+			 * @return the builder's step
+			 */
+			public <S> FirstStep addEmbeddedModel(
+				String key, Class<S> modelClass,
+				Function<T, Optional<S>> modelFunction) {
+
+				_representor._embeddedRelatedModels.add(
+					new RelatedModel<>(key, modelClass, modelFunction));
+
+				return this;
+			}
+
+			/**
+			 * Adds information about a resource link.
+			 *
+			 * @param  key the field's name
+			 * @param  url the link's URL
+			 * @return the builder's step
+			 */
+			public FirstStep addLink(String key, String url) {
+				_representor._links.put(key, url);
+
+				return this;
+			}
+
+			/**
+			 * Adds information about a non-embeddable related model.
+			 *
+			 * @param  key the relation's name
+			 * @param  modelClass the related model's class
+			 * @param  modelFunction the function used to get the related model
+			 * @return the builder's step
+			 */
+			public <S> FirstStep addLinkedModel(
+				String key, Class<S> modelClass,
+				Function<T, Optional<S>> modelFunction) {
+
+				_representor._linkedRelatedModels.add(
+					new RelatedModel<>(key, modelClass, modelFunction));
+
+				return this;
+			}
+
+			/**
+			 * Provides information about a resource localized string field.
+			 *
+			 * @param  key the field's name
+			 * @param  stringFunction the function used to get the string value
+			 * @return builder's step
+			 */
+			public FirstStep addLocalizedString(
+				String key, BiFunction<T, Language, String> stringFunction) {
+
+				_representor._localizedStringFunctions.put(key, stringFunction);
+
+				return this;
+			}
+
+			/**
+			 * Adds information about a resource's number field.
+			 *
+			 * @param  key the field's name
+			 * @param  numberFunction the function used to get the number's
+			 *         value
+			 * @return the builder's step
+			 */
+			public FirstStep addNumber(
+				String key, Function<T, Number> numberFunction) {
+
+				_representor._numberFunctions.put(key, numberFunction);
+
+				return this;
+			}
+
+			/**
+			 * Adds information about a related collection.
+			 *
+			 * @param  key the relation's name
+			 * @param  modelClass the class of the collection's related models
+			 * @param  identifierFunction the function used to get the
+			 *         collection's identifier
+			 * @return the builder's step
+			 */
+			public <S> FirstStep addRelatedCollection(
+				String key, Class<S> modelClass,
+				Function<T, Identifier> identifierFunction) {
+
+				_representor._relatedCollections.add(
+					new RelatedCollection<>(
+						key, modelClass, identifierFunction));
+
+				return this;
+			}
+
+			/**
+			 * Adds information about a resource's string field.
+			 *
+			 * @param  key the field's name
+			 * @param  stringFunction the function used to get the string's
+			 *         value
+			 * @return the builder's step
+			 */
+			public FirstStep addString(
+				String key, Function<T, String> stringFunction) {
+
+				_representor._stringFunctions.put(key, stringFunction);
+
+				return this;
+			}
+
+			/**
+			 * Adds a type for the model. Multiple types are allowed.
+			 *
+			 * @param  type the type name
+			 * @return the builder's step
+			 */
+			public FirstStep addType(String type) {
+				_representor._types.add(type);
+
+				return this;
+			}
+
+			/**
+			 * Constructs and returns a {@link Representor} instance with the
+			 * information provided to the builder.
+			 *
+			 * @return the {@code Representor} instance
+			 */
+			public Representor<T, U> build() {
+				return _representor;
+			}
+
+		}
+
+		private final TriConsumer<String, Class<?>,
+			Function<Object, Identifier>> _addRelatedCollectionTriConsumer;
+		private final Representor<T, U> _representor;
+
+	}
+
+	private Map<String, BinaryFunction<T>> _binaryFunctions = new HashMap<>();
+	private Map<String, Function<T, Boolean>> _booleanFunctions =
+		new HashMap<>();
+	private List<RelatedModel<T, ?>> _embeddedRelatedModels = new ArrayList<>();
+	private final Class<U> _identifierClass;
+	private Function<T, U> _identifierFunction;
+	private List<RelatedModel<T, ?>> _linkedRelatedModels = new ArrayList<>();
+	private Map<String, String> _links = new HashMap<>();
+	private Map<String, BiFunction<T, Language, String>>
+		_localizedStringFunctions = new HashMap<>();
+	private Map<String, Function<T, Number>> _numberFunctions = new HashMap<>();
+	private List<RelatedCollection<T, ?>> _relatedCollections =
+		new ArrayList<>();
+	private final Supplier<List<RelatedCollection<T, ?>>>
+		_relatedCollectionsSupplier;
+	private Map<String, Function<T, String>> _stringFunctions = new HashMap<>();
+	private List<String> _types = new ArrayList<>();
 
 }
