e3472c83b1b58dd35e7b1602ad6fb2d6b61941c5
Alejandro Hernández <info@alejandrohdezma.com>
2017-12-27 13:14:41 +0100

    APIO-75 Create RoutesBuilderUtilTest

---
+++ apio-architect-api/src/test/java/com/liferay/apio/architect/routes/RoutesBuilderUtilTest.java
@@ -0,0 +1,265 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.routes;
+
+import static com.liferay.apio.architect.routes.RoutesBuilderUtil.provide;
+import static com.liferay.apio.architect.routes.RoutesBuilderUtil.provideConsumer;
+import static com.liferay.apio.architect.routes.RoutesTestUtil.PROVIDE_FUNCTION;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import com.liferay.apio.architect.error.ApioDeveloperError.MustHaveProvider;
+import com.liferay.apio.architect.language.Language;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import org.junit.Test;
+
+/**
+ * @author Alejandro Hernández
+ */
+public class RoutesBuilderUtilTest {
+
+	@Test(expected = UnsupportedOperationException.class)
+	public void testConstructorThrowsException() throws Throwable {
+		Constructor<?> constructor =
+			RoutesBuilderUtil.class.getDeclaredConstructors()[0];
+
+		constructor.setAccessible(true);
+
+		try {
+			constructor.newInstance();
+		}
+		catch (InvocationTargetException ite) {
+			throw ite.getTargetException();
+		}
+	}
+
+	@Test(expected = MustHaveProvider.class)
+	public void testFiveParameterProvideConsumerMethodFailsIfOptionalEmpty() {
+		provideConsumer(
+			PROVIDE_FUNCTION, null, Language.class, Long.class, Integer.class,
+			Boolean.class,
+			string -> aLong -> integer -> aBoolean -> {
+				throw new AssertionError("This lambda should not be called");
+			});
+	}
+
+	@Test(expected = MustHaveProvider.class)
+	public void testFiveParameterProvideMethodFailsIfOptionalEmpty() {
+		provide(
+			PROVIDE_FUNCTION, null, Language.class, Long.class, Integer.class,
+			Boolean.class, Float.class,
+			string -> aLong -> integer -> aBoolean -> list -> {
+				throw new AssertionError("This lambda should not be called");
+			});
+	}
+
+	@Test
+	public void testFiveParameterProvideMethodProvides() {
+		String result = provide(
+			PROVIDE_FUNCTION, null, String.class, Long.class, Integer.class,
+			Boolean.class, Float.class,
+			string -> aLong -> integer -> aBoolean -> aFloat -> {
+				assertThat(string, is(equalTo("Apio")));
+				assertThat(aLong, is(equalTo(42L)));
+				assertThat(integer, is(equalTo(2017)));
+				assertThat(aBoolean, is(true));
+				assertThat(aFloat, is(equalTo(0.1F)));
+
+				return "The result";
+			});
+
+		assertThat(result, is(equalTo("The result")));
+	}
+
+	@Test(expected = MustHaveProvider.class)
+	public void testFourParameterProvideConsumerMethodFailsIfOptionalEmpty() {
+		provideConsumer(
+			PROVIDE_FUNCTION, null, Language.class, Long.class, Integer.class,
+			Boolean.class,
+			string -> aLong -> integer -> aBoolean -> {
+				throw new AssertionError("This lambda should not be called");
+			});
+	}
+
+	@Test
+	public void testFourParameterProvideConsumerMethodProvides() {
+		provideConsumer(
+			PROVIDE_FUNCTION, null, String.class, Long.class, Integer.class,
+			Boolean.class,
+			string -> aLong -> integer -> aBoolean -> {
+				assertThat(string, is(equalTo("Apio")));
+				assertThat(aLong, is(equalTo(42L)));
+				assertThat(integer, is(equalTo(2017)));
+				assertThat(aBoolean, is(true));
+			});
+	}
+
+	@Test(expected = MustHaveProvider.class)
+	public void testFourParameterProvideMethodFailsIfOptionalEmpty() {
+		provide(
+			PROVIDE_FUNCTION, null, Language.class, Long.class, Integer.class,
+			Boolean.class,
+			string -> aLong -> integer -> aBoolean -> {
+				throw new AssertionError("This lambda should not be called");
+			});
+	}
+
+	@Test
+	public void testFourParameterProvideMethodProvides() {
+		String result = provide(
+			PROVIDE_FUNCTION, null, String.class, Long.class, Integer.class,
+			Boolean.class,
+			string -> aLong -> integer -> aBoolean -> {
+				assertThat(string, is(equalTo("Apio")));
+				assertThat(aLong, is(equalTo(42L)));
+				assertThat(integer, is(equalTo(2017)));
+				assertThat(aBoolean, is(true));
+
+				return "The result";
+			});
+
+		assertThat(result, is(equalTo("The result")));
+	}
+
+	@Test(expected = MustHaveProvider.class)
+	public void testOneParameterProvideConsumerMethodFailsIfOptionalEmpty() {
+		provideConsumer(
+			PROVIDE_FUNCTION, null, Language.class,
+			string -> {
+				throw new AssertionError("This lambda should not be called");
+			});
+	}
+
+	@Test
+	public void testOneParameterProvideConsumerMethodProvides() {
+		provideConsumer(
+			PROVIDE_FUNCTION, null, String.class,
+			string -> assertThat(string, is(equalTo("Apio"))));
+	}
+
+	@Test(expected = MustHaveProvider.class)
+	public void testOneParameterProvideMethodFailsIfOptionalEmpty() {
+		provide(
+			PROVIDE_FUNCTION, null, Language.class,
+			string -> {
+				throw new AssertionError("This lambda should not be called");
+			});
+	}
+
+	@Test
+	public void testOneParameterProvideMethodProvides() {
+		String result = provide(
+			PROVIDE_FUNCTION, null, String.class,
+			string -> {
+				assertThat(string, is(equalTo("Apio")));
+
+				return "The result";
+			});
+
+		assertThat(result, is(equalTo("The result")));
+	}
+
+	@Test(expected = MustHaveProvider.class)
+	public void testThreeParameterProvideConsumerMethodFailsIfOptionalEmpty() {
+		provideConsumer(
+			PROVIDE_FUNCTION, null, Language.class, Long.class, Integer.class,
+			string -> aLong -> integer -> {
+				throw new AssertionError("This lambda should not be called");
+			});
+	}
+
+	@Test
+	public void testThreeParameterProvideConsumerMethodProvides() {
+		provideConsumer(
+			PROVIDE_FUNCTION, null, String.class, Long.class, Integer.class,
+			string -> aLong -> integer -> {
+				assertThat(string, is(equalTo("Apio")));
+				assertThat(aLong, is(equalTo(42L)));
+				assertThat(integer, is(equalTo(2017)));
+			});
+	}
+
+	@Test(expected = MustHaveProvider.class)
+	public void testThreeParameterProvideMethodFailsIfOptionalEmpty() {
+		provide(
+			PROVIDE_FUNCTION, null, Language.class, Long.class, Integer.class,
+			string -> aLong -> integer -> {
+				throw new AssertionError("This lambda should not be called");
+			});
+	}
+
+	@Test
+	public void testThreeParameterProvideMethodProvides() {
+		String result = provide(
+			PROVIDE_FUNCTION, null, String.class, Long.class, Integer.class,
+			string -> aLong -> integer -> {
+				assertThat(string, is(equalTo("Apio")));
+				assertThat(aLong, is(equalTo(42L)));
+				assertThat(integer, is(equalTo(2017)));
+
+				return "The result";
+			});
+
+		assertThat(result, is(equalTo("The result")));
+	}
+
+	@Test(expected = MustHaveProvider.class)
+	public void testTwoParameterProvideConsumerMethodFailsIfOptionalEmpty() {
+		provideConsumer(
+			PROVIDE_FUNCTION, null, Language.class, Long.class,
+			string -> aLong -> {
+				throw new AssertionError("This lambda should not be called");
+			});
+	}
+
+	@Test
+	public void testTwoParameterProvideConsumerMethodProvides() {
+		provideConsumer(
+			PROVIDE_FUNCTION, null, String.class, Long.class,
+			string -> aLong -> {
+				assertThat(string, is(equalTo("Apio")));
+				assertThat(aLong, is(equalTo(42L)));
+			});
+	}
+
+	@Test(expected = MustHaveProvider.class)
+	public void testTwoParameterProvideMethodFailsIfOptionalEmpty() {
+		provide(
+			PROVIDE_FUNCTION, null, Language.class, Long.class,
+			string -> aLong -> {
+				throw new AssertionError("This lambda should not be called");
+			});
+	}
+
+	@Test
+	public void testTwoParameterProvideMethodProvides() {
+		String result = provide(
+			PROVIDE_FUNCTION, null, String.class, Long.class,
+			string -> aLong -> {
+				assertThat(string, is(equalTo("Apio")));
+				assertThat(aLong, is(equalTo(42L)));
+
+				return "The result";
+			});
+
+		assertThat(result, is(equalTo("The result")));
+	}
+
+}
---
+++ apio-architect-api/src/test/java/com/liferay/apio/architect/routes/RoutesTestUtil.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.routes;
+
+import com.liferay.apio.architect.alias.ProvideFunction;
+import com.liferay.apio.architect.pagination.Pagination;
+
+import java.util.Optional;
+
+/**
+ * Provides utilities for testing routes classes.
+ *
+ * @author Alejandro Hernández
+ */
+public class RoutesTestUtil {
+
+	/**
+	 * Mocked {@code Pagination}.
+	 */
+	public static final Pagination PAGINATION = new Pagination(4, 2);
+
+	/**
+	 * A {@code ProvideFunction} that is able to provide instances of {@code
+	 * String}, {@code Long}, {@code Integer}, {@code Boolean}, {@code Float}
+	 * and {@code Pagination}.
+	 */
+	public static final ProvideFunction PROVIDE_FUNCTION =
+		httpServletRequest -> aClass -> {
+			if (aClass.equals(String.class)) {
+				return Optional.of("Apio");
+			}
+			else if (aClass.equals(Long.class)) {
+				return Optional.of(42L);
+			}
+			else if (aClass.equals(Integer.class)) {
+				return Optional.of(2017);
+			}
+			else if (aClass.equals(Boolean.class)) {
+				return Optional.of(true);
+			}
+			else if (aClass.equals(Float.class)) {
+				return Optional.of(0.1F);
+			}
+			else if (aClass.equals(Pagination.class)) {
+				return Optional.of(PAGINATION);
+			}
+			else {
+				return Optional.empty();
+			}
+		};
+
+}
