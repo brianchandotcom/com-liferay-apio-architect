d68ccb0b3ef8333c67cf775abe4b63574540bde2
Alejandro Hernández <info@alejandrohdezma.com>
2018-01-04 12:37:10 +0100

    APIO-75 Create FormMessageMapper for JSON-LD + HYDRA

---
+++ apio-architect-message-json-ld/src/main/java/com/liferay/apio/architect/message/json/ld/internal/JSONLDFormMessageMapper.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.message.json.ld.internal;
+
+import com.liferay.apio.architect.form.Form;
+import com.liferay.apio.architect.message.json.FormMessageMapper;
+import com.liferay.apio.architect.message.json.JSONObjectBuilder;
+
+import javax.ws.rs.core.HttpHeaders;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Represents forms in JSON-LD + Hydra format.
+ *
+ * <p>
+ * For more information, see <a href="https://json-ld.org/">JSON-LD </a> and <a
+ * href="https://www.hydra-cg.com/">Hydra </a> .
+ * </p>
+ *
+ * @author Alejandro Hernández
+ */
+@Component(immediate = true)
+public class JSONLDFormMessageMapper implements FormMessageMapper {
+
+	@Override
+	public String getMediaType() {
+		return "application/ld+json";
+	}
+
+	@Override
+	public void mapFormURL(JSONObjectBuilder jsonObjectBuilder, String url) {
+		jsonObjectBuilder.field(
+			"@id"
+		).stringValue(
+			url
+		);
+	}
+
+	@Override
+	public void onFinish(
+		JSONObjectBuilder jsonObjectBuilder, Form form,
+		HttpHeaders httpHeaders) {
+
+		jsonObjectBuilder.field(
+			"@type"
+		).stringValue(
+			"Class"
+		);
+
+		jsonObjectBuilder.field(
+			"@context"
+		).stringValue(
+			"http://www.w3.org/ns/hydra/context.jsonld"
+		);
+	}
+
+}
