41438d5ba606b135fdc266fc003af1d220751bd6
Alejandro Hernández <info@alejandrohdezma.com>
2018-01-05 10:42:58 +0100

    APIO-108 Extract Plain JSON SingleModel JsonObject Matchers from Test to Util

--- apio-architect-message-json-plain/src/test/java/com/liferay/apio/architect/message/json/plain/internal/PlainJSONSingleModelMessageMapperTest.java
+++ apio-architect-message-json-plain/src/test/java/com/liferay/apio/architect/message/json/plain/internal/PlainJSONSingleModelMessageMapperTest.java
@@ -14,26 +14,18 @@
 
 package com.liferay.apio.architect.message.json.plain.internal;
 
-import static com.liferay.apio.architect.message.json.plain.internal.PlainJSONTestUtil.isALinkTo;
-import static com.liferay.apio.architect.test.json.JsonMatchers.aJsonBoolean;
-import static com.liferay.apio.architect.test.json.JsonMatchers.aJsonInt;
-import static com.liferay.apio.architect.test.json.JsonMatchers.aJsonObjectWith;
-import static com.liferay.apio.architect.test.json.JsonMatchers.aJsonString;
+import static com.liferay.apio.architect.message.json.plain.internal.PlainJSONTestUtil.aRootElementJsonObjectWithId;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
-import com.liferay.apio.architect.test.json.Conditions;
 import com.liferay.apio.architect.test.model.RootModel;
 import com.liferay.apio.architect.test.writer.MockSingleModelWriter;
 
 import javax.ws.rs.core.HttpHeaders;
-
-import org.hamcrest.Matcher;
 
 import org.junit.Test;
 
@@ -59,110 +51,7 @@ public class PlainJSONSingleModelMessageMapperTest {
 		JsonObject jsonObject = MockSingleModelWriter.write(
 			httpHeaders, _singleModelMessageMapper);
 
-		Conditions.Builder builder = new Conditions.Builder();
-
-		Conditions conditions = builder.where(
-			"binary1", isALinkTo("localhost/b/model/first/binary1")
-		).where(
-			"binary2", isALinkTo("localhost/b/model/first/binary2")
-		).where(
-			"boolean1", is(aJsonBoolean(true))
-		).where(
-			"boolean2", is(aJsonBoolean(false))
-		).where(
-			"date1", is(aJsonString(equalTo("2016-06-15T09:00Z")))
-		).where(
-			"date2", is(aJsonString(equalTo("2017-04-03T18:36Z")))
-		).where(
-			"embedded1", _isAJsonObjectWithTheFirstEmbedded
-		).where(
-			"embedded2", isALinkTo("localhost/p/first-inner-model/second")
-		).where(
-			"link1", isALinkTo("www.liferay.com")
-		).where(
-			"link2", isALinkTo("community.liferay.com")
-		).where(
-			"linked1", isALinkTo("localhost/p/first-inner-model/third")
-		).where(
-			"linked2", isALinkTo("localhost/p/first-inner-model/fourth")
-		).where(
-			"localizedString1", is(aJsonString(equalTo("Translated 1")))
-		).where(
-			"localizedString2", is(aJsonString(equalTo("Translated 2")))
-		).where(
-			"number1", is(aJsonInt(equalTo(2017)))
-		).where(
-			"number2", is(aJsonInt(equalTo(42)))
-		).where(
-			"relatedCollection1", isALinkTo("localhost/p/model/first/models")
-		).where(
-			"relatedCollection2", isALinkTo("localhost/p/model/first/models")
-		).where(
-			"self", isALinkTo("localhost/p/model/first")
-		).where(
-			"string1", is(aJsonString(equalTo("Live long and prosper")))
-		).where(
-			"string2", is(aJsonString(equalTo("Hypermedia")))
-		).build();
-
-		assertThat(jsonObject, is(aJsonObjectWith(conditions)));
-	}
-
-	private static final Matcher<JsonElement>
-		_isAJsonObjectWithTheFirstEmbedded;
-
-	static {
-		Conditions.Builder builder = new Conditions.Builder();
-
-		Conditions secondEmbeddedConditions = builder.where(
-			"binary", isALinkTo("localhost/b/second-inner-model/first/binary")
-		).where(
-			"boolean", is(aJsonBoolean(false))
-		).where(
-			"embedded", isALinkTo("localhost/p/third-inner-model/first")
-		).where(
-			"link", isALinkTo("community.liferay.com")
-		).where(
-			"number", is(aJsonInt(equalTo(2017)))
-		).where(
-			"relatedCollection",
-			isALinkTo("localhost/p/second-inner-model/first/models")
-		).where(
-			"self", isALinkTo("localhost/p/second-inner-model/first")
-		).where(
-			"string", is(aJsonString(equalTo("A string")))
-		).where(
-			"linked", isALinkTo("localhost/p/third-inner-model/second")
-		).build();
-
-		Matcher<JsonElement> isAJsonObjectWithTheSecondEmbedded = is(
-			aJsonObjectWith(secondEmbeddedConditions));
-
-		Conditions firstEmbeddedConditions = builder.where(
-			"binary", isALinkTo("localhost/b/first-inner-model/first/binary")
-		).where(
-			"boolean", is(aJsonBoolean(true))
-		).where(
-			"link", isALinkTo("www.liferay.com")
-		).where(
-			"localizedString", is(aJsonString(equalTo("Translated")))
-		).where(
-			"number", is(aJsonInt(equalTo(42)))
-		).where(
-			"relatedCollection",
-			isALinkTo("localhost/p/first-inner-model/first/models")
-		).where(
-			"embedded", isAJsonObjectWithTheSecondEmbedded
-		).where(
-			"linked", isALinkTo("localhost/p/second-inner-model/second")
-		).where(
-			"self", isALinkTo("localhost/p/first-inner-model/first")
-		).where(
-			"string", is(aJsonString(equalTo("A string")))
-		).build();
-
-		_isAJsonObjectWithTheFirstEmbedded = is(
-			aJsonObjectWith(firstEmbeddedConditions));
+		assertThat(jsonObject, is(aRootElementJsonObjectWithId("first")));
 	}
 
 	private final PlainJSONSingleModelMessageMapper<RootModel>
--- apio-architect-message-json-plain/src/test/java/com/liferay/apio/architect/message/json/plain/internal/PlainJSONTestUtil.java
+++ apio-architect-message-json-plain/src/test/java/com/liferay/apio/architect/message/json/plain/internal/PlainJSONTestUtil.java
@@ -14,12 +14,18 @@
 
 package com.liferay.apio.architect.message.json.plain.internal;
 
+import static com.liferay.apio.architect.test.json.JsonMatchers.aJsonBoolean;
+import static com.liferay.apio.architect.test.json.JsonMatchers.aJsonInt;
+import static com.liferay.apio.architect.test.json.JsonMatchers.aJsonObjectWith;
 import static com.liferay.apio.architect.test.json.JsonMatchers.aJsonString;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.core.Is.is;
 
 import com.google.gson.JsonElement;
+
+import com.liferay.apio.architect.test.json.Conditions;
+import com.liferay.apio.architect.test.json.Conditions.Builder;
 
 import org.hamcrest.Matcher;
 
@@ -35,7 +41,139 @@ import org.hamcrest.Matcher;
 public class PlainJSONTestUtil {
 
 	/**
-	 * Returns a matcher that checks if the field is a link to the provided URL.
+	 * Returns a {@link Matcher} that checks if the field is the JSON Object of
+	 * a {@code RootElement} with the provided ID.
+	 *
+	 * @param  id the ID of the {@code RootElement}
+	 * @return a matcher for a JSON Object of a {@code RootElement} with the
+	 *         provided ID
+	 * @review
+	 */
+	public static Matcher<JsonElement> aRootElementJsonObjectWithId(String id) {
+		Builder builder = new Builder();
+
+		Conditions conditions = builder.where(
+			"binary1", isALinkTo("localhost/b/model/" + id + "/binary1")
+		).where(
+			"binary2", isALinkTo("localhost/b/model/" + id + "/binary2")
+		).where(
+			"boolean1", is(aJsonBoolean(true))
+		).where(
+			"boolean2", is(aJsonBoolean(false))
+		).where(
+			"date1", is(aJsonString(equalTo("2016-06-15T09:00Z")))
+		).where(
+			"date2", is(aJsonString(equalTo("2017-04-03T18:36Z")))
+		).where(
+			"embedded1", isAJsonObjectWithTheFirstEmbedded()
+		).where(
+			"embedded2", isALinkTo("localhost/p/first-inner-model/second")
+		).where(
+			"link1", isALinkTo("www.liferay.com")
+		).where(
+			"link2", isALinkTo("community.liferay.com")
+		).where(
+			"linked1", isALinkTo("localhost/p/first-inner-model/third")
+		).where(
+			"linked2", isALinkTo("localhost/p/first-inner-model/fourth")
+		).where(
+			"localizedString1", is(aJsonString(equalTo("Translated 1")))
+		).where(
+			"localizedString2", is(aJsonString(equalTo("Translated 2")))
+		).where(
+			"number1", is(aJsonInt(equalTo(2017)))
+		).where(
+			"number2", is(aJsonInt(equalTo(42)))
+		).where(
+			"relatedCollection1",
+			isALinkTo("localhost/p/model/" + id + "/models")
+		).where(
+			"relatedCollection2",
+			isALinkTo("localhost/p/model/" + id + "/models")
+		).where(
+			"self", isALinkTo("localhost/p/model/" + id)
+		).where(
+			"string1", is(aJsonString(equalTo("Live long and prosper")))
+		).where(
+			"string2", is(aJsonString(equalTo("Hypermedia")))
+		).build();
+
+		return is(aJsonObjectWith(conditions));
+	}
+
+	/**
+	 * Returns a {@link Matcher} that checks if the field is a JSON Object of
+	 * the first embedded.
+	 *
+	 * @return a matcher for a JSON Object of the first embedded
+	 * @review
+	 */
+	public static Matcher<JsonElement> isAJsonObjectWithTheFirstEmbedded() {
+		Conditions.Builder builder = new Conditions.Builder();
+
+		Conditions firstEmbeddedConditions = builder.where(
+			"binary", isALinkTo("localhost/b/first-inner-model/first/binary")
+		).where(
+			"boolean", is(aJsonBoolean(true))
+		).where(
+			"link", isALinkTo("www.liferay.com")
+		).where(
+			"number", is(aJsonInt(equalTo(42)))
+		).where(
+			"relatedCollection",
+			isALinkTo("localhost/p/first-inner-model/first/models")
+		).where(
+			"embedded", isAJsonObjectWithTheSecondEmbedded()
+		).where(
+			"linked", isALinkTo("localhost/p/second-inner-model/second")
+		).where(
+			"localizedString", is(aJsonString(equalTo("Translated")))
+		).where(
+			"self", isALinkTo("localhost/p/first-inner-model/first")
+		).where(
+			"string", is(aJsonString(equalTo("A string")))
+		).build();
+
+		return is(aJsonObjectWith(firstEmbeddedConditions));
+	}
+
+	/**
+	 * Returns a {@link Matcher} that checks if the field is a JSON Object of
+	 * the second embedded.
+	 *
+	 * @return a matcher for a JSON Object of the second embedded
+	 * @review
+	 */
+	public static Matcher<JsonElement> isAJsonObjectWithTheSecondEmbedded() {
+		Builder builder = new Builder();
+
+		Conditions secondEmbeddedConditions = builder.where(
+			"binary", isALinkTo("localhost/b/second-inner-model/first/binary")
+		).where(
+			"boolean", is(aJsonBoolean(false))
+		).where(
+			"embedded", isALinkTo("localhost/p/third-inner-model/first")
+		).where(
+			"link", isALinkTo("community.liferay.com")
+		).where(
+			"number", is(aJsonInt(equalTo(2017)))
+		).where(
+			"relatedCollection",
+			isALinkTo("localhost/p/second-inner-model/first/models")
+		).where(
+			"self", isALinkTo("localhost/p/second-inner-model/first")
+		).where(
+			"string", is(aJsonString(equalTo("A string")))
+		).where(
+			"linked", isALinkTo("localhost/p/third-inner-model/second")
+		).build();
+
+		return is(aJsonObjectWith(secondEmbeddedConditions));
+	}
+
+	/**
+	 * Returns a {@link Matcher} that checks if the field is a link to the
+	 * provided URL.
 	 *
 	 * @param  url the URL to match
 	 * @return a matcher for URL fields
