f416875bd6bb0b4080d9ae98ac3eae58f1c037c0
Alejandro Hernández <info@alejandrohdezma.com>
2018-01-09 18:58:24 +0100

    APIO-75 Add tests for sample forms

---
+++ apio-architect-sample/src/test/java/com/liferay/apio/architect/sample/internal/form/BlogPostingCommentUpdaterFormTest.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.sample.internal.form;
+
+import static com.liferay.apio.architect.form.Form.Builder.empty;
+import static com.liferay.apio.architect.sample.internal.form.BlogPostingCommentUpdaterForm.buildForm;
+import static com.liferay.apio.architect.test.form.FormMatchers.isAFormWithConditions;
+import static com.liferay.apio.architect.test.form.FormMatchers.isReturnedIn;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+/**
+ * @author Alejandro Hernández
+ */
+public class BlogPostingCommentUpdaterFormTest {
+
+	@Test
+	public void test() {
+		assertThat(
+			buildForm(empty()),
+			isAFormWithConditions(
+				builder -> builder.whereString(
+					"text", isReturnedIn(BlogPostingCommentUpdaterForm::getText)
+				).build()));
+	}
+
+}
---
+++ apio-architect-sample/src/test/java/com/liferay/apio/architect/sample/internal/form/PersonFormTest.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.sample.internal.form;
+
+import static com.liferay.apio.architect.form.Form.Builder.empty;
+import static com.liferay.apio.architect.sample.internal.form.PersonForm.buildForm;
+import static com.liferay.apio.architect.test.form.FormMatchers.isAFormWithConditions;
+import static com.liferay.apio.architect.test.form.FormMatchers.isReturnedIn;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com.liferay.apio.architect.form.Form;
+
+import org.hamcrest.Matcher;
+
+import org.junit.Test;
+
+/**
+ * @author Alejandro Hernández
+ */
+public class PersonFormTest {
+
+	@Test
+	public void test() {
+		Form<PersonForm> form = buildForm(empty());
+
+		Matcher<Form<PersonForm>> isAFormWithConditions = isAFormWithConditions(
+			builder -> builder.whereDate(
+				"birthDate", isReturnedIn(PersonForm::getBirthDate)
+			).whereString(
+				"address", isReturnedIn(PersonForm::getAddress)
+			).whereString(
+				"email", isReturnedIn(PersonForm::getEmail)
+			).whereString(
+				"familyName", isReturnedIn(PersonForm::getFamilyName)
+			).whereString(
+				"givenName", isReturnedIn(PersonForm::getGivenName)
+			).whereString(
+				"image", isReturnedIn(PersonForm::getImage)
+			).whereString(
+				"jobTitle", isReturnedIn(PersonForm::getJobTitle)
+			).build());
+
+		assertThat(form, isAFormWithConditions);
+	}
+
+}
