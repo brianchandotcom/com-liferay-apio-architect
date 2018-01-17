b6651d710d9c2fa4993e7a1949843e95496a0364
Alejandro Hernández <info@alejandrohdezma.com>
2018-01-17 11:08:40 +0100

    APIO-75 Create alias for Page and NestedPage operations functions

---
+++ apio-architect-writer-api/src/main/java/com/liferay/apio/architect/writer/alias/PageOperationsFunction.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.writer.alias;
+
+import com.liferay.apio.architect.operation.Operation;
+
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * Defines a type alias for a function that receives a page model class and
+ * returns a list of the page operations.
+ *
+ * @author Alejandro Hernández
+ * @review
+ */
+@FunctionalInterface
+public interface PageOperationsFunction
+	extends Function<Class<?>, List<Operation>> {
+}
