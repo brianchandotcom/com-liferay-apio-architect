103d12520cfa004939e081b3d483f7d4e956417b
Alejandro Hernández <info@alejandrohdezma.com>
2018-01-30 11:52:00 +0100

    APIO-112 Update RelatedCollection so collections are added via Identifier now

--- apio-architect-api/src/main/java/com/liferay/apio/architect/related/RelatedCollection.java
+++ apio-architect-api/src/main/java/com/liferay/apio/architect/related/RelatedCollection.java
@@ -14,6 +14,8 @@
 
 package com.liferay.apio.architect.related;
 
+import com.liferay.apio.architect.identifier.Identifier;
+
 import java.util.function.Function;
 
 /**
@@ -21,26 +23,50 @@ import java.util.function.Function;
  *
  * @author Alejandro Hernández
  * @param  <T> the model's type
- * @param  <S> the type of the related collection's model
+ * @param  <S> the type of the related collection item's identifier (e.g.,
+ *         {@code Long}, {@code String}, etc.)
+ * @param  <U> the type of the related collection's identifier. It must be a
+ *         subclass of {@code Identifier<S>}.
+ * @review
  */
-public class RelatedCollection<T, S> {
+public class RelatedCollection<T, S extends Identifier, U> {
 
 	public RelatedCollection(
-		String key, Class<S> modelClass, Function<T, ?> identifierFunction) {
+		String key, Class<S> itemIdentifierClass,
+		Class<? extends Identifier<U>> collectionIdentifierClass,
+		Function<T, U> identifierFunction) {
 
 		_key = key;
-		_modelClass = modelClass;
+		_itemIdentifierClass = itemIdentifierClass;
+		_collectionIdentifierClass = collectionIdentifierClass;
 		_identifierFunction = identifierFunction;
 	}
 
 	/**
-	 * Returns the function you can use to create the related collection's
-	 * identifier.
+	 * Returns the class of the collection items' identifier.
+	 *
+	 * @return the class of the collection items' identifier
+	 */
+	public Class<? extends Identifier<U>> getCollectionIdentifierClass() {
+		return _collectionIdentifierClass;
+	}
+
+	/**
+	 * Returns the function that calculates the related collection's identifier.
 	 *
 	 * @return the function that calculates the related collection's identifier
 	 */
-	public Function<T, ?> getIdentifierFunction() {
+	public Function<T, U> getIdentifierFunction() {
 		return _identifierFunction;
+	}
+
+	/**
+	 * Returns the class of the collection items' identifier.
+	 *
+	 * @return the class of the collection items' identifier
+	 */
+	public Class<S> getItemIdentifierClass() {
+		return _itemIdentifierClass;
 	}
 
 	/**
@@ -52,17 +78,9 @@ public class RelatedCollection<T, S> {
 		return _key;
 	}
 
-	/**
-	 * Returns the class of the collection's related models.
-	 *
-	 * @return the class of the collection's related models
-	 */
-	public Class<S> getModelClass() {
-		return _modelClass;
-	}
-
-	private final Function<T, ?> _identifierFunction;
+	private final Class<? extends Identifier<U>> _collectionIdentifierClass;
+	private final Function<T, U> _identifierFunction;
+	private final Class<S> _itemIdentifierClass;
 	private final String _key;
-	private final Class<S> _modelClass;
 
 }
