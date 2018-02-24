b4f0f046cab068fbaf63386460d08109ff1d6270
Javier Gamarra <nhpatt@gmail.com>
2018-02-24 20:29:18 +0100

    APIO-83 add binary file to store mime type

---
+++ apio-architect-api/src/main/java/com/liferay/apio/architect/file/BinaryFile.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.file;
+
+import java.io.InputStream;
+
+/**
+ * @author Javier Gamarra
+ */
+public class BinaryFile {
+
+	public BinaryFile(InputStream inputStream, String mimeType) {
+		_inputStream = inputStream;
+		_mimeType = mimeType;
+	}
+
+	public InputStream getInputStream() {
+		return _inputStream;
+	}
+
+	public String getMimeType() {
+		return _mimeType;
+	}
+
+	private final InputStream _inputStream;
+	private final String _mimeType;
+
+}
