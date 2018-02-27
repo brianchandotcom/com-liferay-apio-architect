e898cdc02b140aaa0623362ed1e25ab2a32f2d65
Alejandro Hernández <info@alejandrohdezma.com>
2018-02-27 19:17:17 +0100

    APIO-116 Add NoSuchModelExceptionConverter for not-found resources

---
+++ apio-architect-sample-liferay-portal/src/main/java/com/liferay/apio/architect/sample/liferay/portal/internal/converter/NoSuchModelExceptionConverter.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.sample.liferay.portal.internal.converter;
+
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+
+import com.liferay.apio.architect.converter.ExceptionConverter;
+import com.liferay.apio.architect.error.APIError;
+import com.liferay.portal.kernel.exception.NoSuchModelException;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Converts {@code NoSuchModelException} to its {@link APIError} representation.
+ *
+ * @author Alejandro Hernández
+ * @review
+ */
+@Component(immediate = true)
+public class NoSuchModelExceptionConverter
+	implements ExceptionConverter<NoSuchModelException> {
+
+	@Override
+	public APIError convert(NoSuchModelException nsme) {
+		return new APIError(
+			nsme, "Resource not found", "not-found", NOT_FOUND.getStatusCode());
+	}
+
+}
