3ff41376f2b1f5e43f880a34a00a90acc867743e
Alejandro Hernández <info@alejandrohdezma.com>
2018-03-06 15:03:26 +0100

    APIO-124 Revert "APIO-161 Websphere fix is no longer needed"
    This reverts commit 2ff11de612c987e110fad60d794c0781523fb1e9.

---
+++ apio-architect-application/src/main/java/com/liferay/apio/architect/application/internal/websphere/WebsphereApiSupport.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.application.internal.websphere;
+
+import java.io.InputStream;
+
+import java.net.URL;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+
+/**
+ * Add support for some Websphere versions where the {@code
+ * javax.annotation-api.jar} is not correctly provided.
+ *
+ * @author Carlos Sierra Andrés
+ * @review
+ */
+public class WebsphereApiSupport implements BundleActivator {
+
+	public static final String WEBSPHERE_CLASS =
+		"/com/ibm/websphere/product/VersionInfo.class";
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+		if (_isWebSphere()) {
+			Bundle bundle = bundleContext.getBundle();
+
+			URL url = bundle.getResource("javax.annotation-api.jar");
+
+			try (InputStream inputStream = url.openStream()) {
+				_bundle = bundleContext.installBundle(
+					"javax.annotation API", inputStream);
+			}
+		}
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+		try {
+			if (_bundle != null) {
+				_bundle.uninstall();
+			}
+		}
+		catch (BundleException be) {
+			be.printStackTrace();
+		}
+	}
+
+	private boolean _isWebSphere() {
+		return true;
+	}
+
+	private Bundle _bundle;
+
+}
