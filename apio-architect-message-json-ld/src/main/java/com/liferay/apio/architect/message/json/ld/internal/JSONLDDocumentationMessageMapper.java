18428ade09b7b724da1d8b77d13625c6b3c9ca4a
Javier Gamarra <nhpatt@gmail.com>
2018-03-21 15:10:42 +0100

    APIO-17 draft mapper with basic values

--- apio-architect-message-json-ld/src/main/java/com/liferay/apio/architect/message/json/ld/internal/JSONLDDocumentationMessageMapper.java
+++ apio-architect-message-json-ld/src/main/java/com/liferay/apio/architect/message/json/ld/internal/JSONLDDocumentationMessageMapper.java
@@ -23,10 +23,15 @@ import static com.liferay.apio.architect.message.json.ld.internal.JSONLDConstant
 import static com.liferay.apio.architect.message.json.ld.internal.JSONLDConstants.TYPE_API_DOCUMENTATION;
 import static com.liferay.apio.architect.message.json.ld.internal.JSONLDConstants.URL_HYDRA_PROFILE;
 
+import com.liferay.apio.architect.alias.RequestFunction;
 import com.liferay.apio.architect.documentation.Documentation;
+import com.liferay.apio.architect.form.FormField;
 import com.liferay.apio.architect.message.json.DocumentationMessageMapper;
 import com.liferay.apio.architect.message.json.JSONObjectBuilder;
 
+import java.util.Optional;
+
+import javax.ws.rs.HttpMethod;
 import javax.ws.rs.core.HttpHeaders;
 
 import org.osgi.service.component.annotations.Component;
@@ -62,6 +67,91 @@ public class JSONLDDocumentationMessageMapper
 	}
 
 	@Override
+	public void mapFormField(
+		JSONObjectBuilder jsonObjectBuilder, Object name,
+		Optional<FormField> formFieldOptional) {
+
+		jsonObjectBuilder.field(
+			JSONLDConstants.FIELD_NAME_TITLE).stringValue(String.valueOf(name));
+
+		Boolean required =
+			formFieldOptional.map(formField -> formField.required).orElse(
+				false);
+
+		jsonObjectBuilder.field(
+			JSONLDConstants.FIELD_NAME_REQUIRED
+		).booleanValue(
+			required
+		);
+
+		jsonObjectBuilder.field(
+			"readonly"
+		).booleanValue(
+			!formFieldOptional.isPresent()
+		);
+
+		jsonObjectBuilder.field(
+			"writeonly"
+		).booleanValue(
+			false
+		);
+	}
+
+	@Override
+	public void mapOperation(
+		JSONObjectBuilder jsonObjectBuilder, String entity,
+		RequestFunction requestFunction) {
+
+		String httpMethod = requestFunction.getHttpMethod();
+
+		jsonObjectBuilder.field(
+			FIELD_NAME_ID
+		).stringValue(
+			"_:" + entity
+		);
+
+		jsonObjectBuilder.field(
+			FIELD_NAME_TYPE
+		).stringValue(
+			JSONLDConstants.TYPE_OPERATION
+		);
+		jsonObjectBuilder.field(
+			"method"
+		).stringValue(
+			httpMethod
+		);
+
+		String value = _getReturnValue(entity, requestFunction);
+
+		jsonObjectBuilder.field(
+			"returns"
+		).stringValue(
+			value
+		);
+	}
+
+	@Override
+	public void mapResource(
+		JSONObjectBuilder jsonObjectBuilder, String entity) {
+
+		jsonObjectBuilder.field(
+			FIELD_NAME_ID
+		).stringValue(
+			"http://schema.org/" + entity
+		);
+		jsonObjectBuilder.field(
+			FIELD_NAME_TYPE
+		).stringValue(
+			JSONLDConstants.TYPE_CLASS
+		);
+		jsonObjectBuilder.field(
+			JSONLDConstants.FIELD_NAME_TITLE
+		).stringValue(
+			entity
+		);
+	}
+
+	@Override
 	public void mapTitle(JSONObjectBuilder jsonObjectBuilder, String title) {
 		jsonObjectBuilder.field(
 			FIELD_NAME_TITLE
@@ -71,15 +161,55 @@ public class JSONLDDocumentationMessageMapper
 	}
 
 	@Override
+	public void onFinishOperation(
+		JSONObjectBuilder jsonObjectBuilder,
+		JSONObjectBuilder propertyJsonObjectBuilder) {
+
+		jsonObjectBuilder.field(
+			"supportedOperation"
+		).arrayValue(
+		).add(
+			propertyJsonObjectBuilder
+		);
+	}
+
+	@Override
+	public void onFinishProperty(
+		JSONObjectBuilder jsonObjectBuilder,
+		JSONObjectBuilder propertyJsonObjectBuilder) {
+
+		jsonObjectBuilder.field(
+			"supportedProperty"
+		).arrayValue(
+		).add(
+			propertyJsonObjectBuilder
+		);
+	}
+
+	@Override
+	public void onFinishResource(
+		JSONObjectBuilder jsonObjectBuilder,
+		JSONObjectBuilder entityJsonObjectBuilder) {
+
+		jsonObjectBuilder.field(
+			"supportedClass"
+		).arrayValue(
+		).add(
+			entityJsonObjectBuilder
+		);
+	}
+
+	@Override
 	public void onStart(
 		JSONObjectBuilder jsonObjectBuilder, Documentation documentation,
 		HttpHeaders httpHeaders) {
 
-		jsonObjectBuilder.field(
-			FIELD_NAME_CONTEXT
-		).stringValue(
-			URL_HYDRA_PROFILE
-		);
+		JSONObjectBuilder.FieldStep contextBuilder =
+			jsonObjectBuilder.nestedField(FIELD_NAME_CONTEXT);
+
+		JSONObjectBuilder.FieldStep hydra = contextBuilder.field("hydra");
+
+		hydra.stringValue(URL_HYDRA_PROFILE);
 
 		jsonObjectBuilder.field(
 			FIELD_NAME_ID
@@ -92,6 +222,117 @@ public class JSONLDDocumentationMessageMapper
 		).stringValue(
 			TYPE_API_DOCUMENTATION
 		);
+
+		JSONObjectBuilder.FieldStep propertyBuilder =
+			contextBuilder.nestedField("property");
+
+		propertyBuilder.field(
+			FIELD_NAME_ID
+		).stringValue(
+			JSONLDConstants.FIELD_NAME_PROPERTY
+		);
+
+		propertyBuilder.field(
+			FIELD_NAME_TYPE
+		).stringValue(
+			FIELD_NAME_ID
+		);
+
+		contextBuilder.field(
+			"readonly"
+		).stringValue(
+			"hydra:readonly"
+		);
+
+		contextBuilder.field(
+			"writeonly"
+		).stringValue(
+			"hydra:writeonly"
+		);
+
+		contextBuilder.field(
+			"supportedClass"
+		).stringValue(
+			"hydra:supportedClass"
+		);
+
+		contextBuilder.field(
+			"supportedProperty"
+		).stringValue(
+			"hydra:supportedProperty"
+		);
+
+		contextBuilder.field(
+			"supportedOperation"
+		).stringValue(
+			"hydra:supportedOperation"
+		);
+
+		contextBuilder.field(
+			"method"
+		).stringValue(
+			"hydra:method"
+		);
+
+		JSONObjectBuilder.FieldStep expectBuilder = contextBuilder.nestedField(
+			"expect");
+
+		expectBuilder.field(
+			FIELD_NAME_ID
+		).stringValue(
+			"hydra:expect"
+		);
+
+		expectBuilder.field(
+			FIELD_NAME_TYPE
+		).stringValue(
+			FIELD_NAME_ID
+		);
+
+		JSONObjectBuilder.FieldStep returnsBuilder = contextBuilder.nestedField(
+			"returns");
+
+		returnsBuilder.field(
+			FIELD_NAME_ID
+		).stringValue(
+			"hydra:returns"
+		);
+
+		returnsBuilder.field(
+			FIELD_NAME_TYPE
+		).stringValue(
+			FIELD_NAME_ID
+		);
+
+		contextBuilder.field(
+			"statusCodes"
+		).stringValue(
+			"hydra:statusCodes"
+		);
+
+		contextBuilder.field(
+			"code"
+		).stringValue(
+			"hydra:statusCodes"
+		);
+	}
+
+	private String _getReturnValue(
+		String entity, RequestFunction requestFunction) {
+
+		String value = null;
+
+		if (HttpMethod.DELETE.equals(requestFunction.getHttpMethod())) {
+			value = "http://www.w3.org/2002/07/owl#Nothing";
+		}
+		else if (requestFunction.getCollection()) {
+			value = "http://www.w3.org/ns/hydra/core#Collection";
+		}
+		else {
+			value = "http://schema.org/" + entity;
+		}
+
+		return value;
 	}
 
 }
