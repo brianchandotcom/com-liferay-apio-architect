b309898f737b8700e73e01945ec84f0d0603a8f8
Alejandro Hernández <info@alejandrohdezma.com>
2018-05-17 12:59:36 +0200

    LPS-80681 Recover old ExceptionMapper as deprecated so we don't break existing ones

---
+++ apio-architect-api/src/main/java/com/liferay/apio/architect/converter/ExceptionMapper.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.converter;
+
+import aQute.bnd.annotation.ConsumerType;
+
+import com.liferay.apio.architect.error.APIError;
+
+/**
+ * Defines an interface whose instances map between an exception and its {@link
+ * APIError} representation. Use the exception's message to provide meaningful
+ * information about the error.
+ *
+ * @author     Alejandro Hernández
+ * @param      <T> the exception's type
+ * @deprecated As of 1.0.0, use {@link
+ *             com.liferay.apio.architect.exception.mapper.ExceptionMapper}
+ *             instead
+ */
+@ConsumerType
+@Deprecated
+public interface ExceptionMapper<T extends Exception> {
+
+	/**
+	 * Converts an exception to its {@link APIError} representation.
+	 *
+	 * @param  exception the exception to map
+	 * @return the exception's {@link APIError} representation
+	 */
+	public APIError map(T exception);
+
+}
