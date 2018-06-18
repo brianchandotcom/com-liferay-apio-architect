f48f2ec0a673696be634c63f0fbe82cbda17fc7d
Javier Gamarra <nhpatt@gmail.com>
2018-06-18 20:28:51 +0200

    APIO-99 add aliases

---
+++ apio-architect-api/src/main/java/com/liferay/apio/architect/alias/routes/CustomItemFunction.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.alias.routes;
+
+import com.liferay.apio.architect.alias.RequestFunction;
+import com.liferay.apio.architect.form.Body;
+import com.liferay.apio.architect.functional.Try;
+import com.liferay.apio.architect.single.model.SingleModel;
+
+import java.util.function.Function;
+
+/**
+ * Defines a type alias for a function that can be used to delete a collection
+ * item.
+ *
+ * @author Javier Gamarra
+ * @param  <T> the model's type
+ * @param  <S> the type of the model's identifier (e.g., {@code Long}, {@code
+ *         String}, etc.)
+ * @review
+ */
+@FunctionalInterface
+public interface CustomItemFunction<T, S>
+	extends RequestFunction<Function<S, Function<Body, Try<SingleModel<T>>>>> {
+}
