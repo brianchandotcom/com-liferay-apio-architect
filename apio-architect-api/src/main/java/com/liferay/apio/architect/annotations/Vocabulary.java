4341f63705f34cb7940aa3f0c04e17ccfd4d4187
Alejandro Hernández <info@alejandrohdezma.com>
2018-09-19 10:23:44 +0200

    APIO-228 Add Type annotation in Vocabulary
    This annotation will provide a way of indicating that an interface contains information about an API type

--- apio-architect-api/src/main/java/com/liferay/apio/architect/annotations/Vocabulary.java
+++ apio-architect-api/src/main/java/com/liferay/apio/architect/annotations/Vocabulary.java
@@ -14,6 +14,12 @@
 
 package com.liferay.apio.architect.annotations;
 
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
 /**
  * This annotation provides namespace for vocabulary annotations. Use one of the
  * nested annotation.
@@ -21,4 +27,49 @@ package com.liferay.apio.architect.annotations;
  * @author Alejandro Hernández
  */
 public @interface Vocabulary {
+
+	/**
+	 * Provides information about a type. This annotation should always be used
+	 * on an interface.
+	 *
+	 * <p>
+	 * Annotation has attributes to customize the schema URL (<a
+	 * href="https://schema.org">schema.org</a> by default) and the description
+	 * of the type (in the case the type is a custom one).
+	 * </p>
+	 *
+	 * @review
+	 */
+	@Retention(RUNTIME)
+	@Target(TYPE)
+	public @interface Type {
+
+		/**
+		 * Returns the type description, if it is a custom one. Returns empty
+		 * string otherwise.
+		 *
+		 * @return the type description, if it is a custom one; empty string
+		 *         otherwise
+		 * @review
+		 */
+		public String description() default "";
+
+		/**
+		 * Returns the type's schema URL
+		 *
+		 * @return the type's schema URL
+		 * @review
+		 */
+		public String schemaURL() default "https://www.schema.org/";
+
+		/**
+		 * Returns the type's name
+		 *
+		 * @return the type's name
+		 * @review
+		 */
+		public String value();
+
+	}
+
 }
