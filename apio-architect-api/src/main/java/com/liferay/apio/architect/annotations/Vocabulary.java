78b56865c7e4b3e0194c08cf97664b34600d672d
Alejandro Hernández <info@alejandrohdezma.com>
2018-09-19 10:20:52 +0200

    APIO-228 Create Vocabulary namespace annotation
    This annotation will never be used directly, instead it will contain nested annotations for shared vocabularies

---
+++ apio-architect-api/src/main/java/com/liferay/apio/architect/annotations/Vocabulary.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.annotations;
+
+/**
+ * This annotation provides namespace for vocabulary annotations. Use one of the
+ * nested annotation.
+ *
+ * @author Alejandro Hernández
+ */
+public @interface Vocabulary {
+}
