48a93968c988a900a1ef438ed3fa3d2855e0f146
Julio Camarero <julio.camarero@liferay.com>
2018-09-27 14:02:44 +0200

    APIO-258 add HAL Error Message Mapper

---
+++ apio-architect-impl/src/main/java/com/liferay/apio/architect/internal/message/json/hal/HALErrorMessageMapper.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.internal.message.json.hal;
+
+import com.liferay.apio.architect.internal.message.json.ErrorMessageMapper;
+import com.liferay.apio.architect.internal.message.json.JSONObjectBuilder;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Represents errors in HAL format.
+ *
+ * <p>
+ * For more information, see <a
+ * href="http://stateless.co/hal_specification.html">HAL </a>.
+ * </p>
+ *
+ * @author Julio Camarero
+ * @review
+ */
+@Component(service = ErrorMessageMapper.class)
+public class HALErrorMessageMapper implements ErrorMessageMapper {
+
+	@Override
+	public String getMediaType() {
+		return "application/hal+json";
+	}
+
+	@Override
+	public void mapDescription(
+		JSONObjectBuilder jsonObjectBuilder, String description) {
+
+		jsonObjectBuilder.field(
+			"description"
+		).stringValue(
+			description
+		);
+	}
+
+	@Override
+	public void mapStatusCode(
+		JSONObjectBuilder jsonObjectBuilder, Integer statusCode) {
+
+		jsonObjectBuilder.field(
+			"statusCode"
+		).numberValue(
+			statusCode
+		);
+	}
+
+	@Override
+	public void mapTitle(JSONObjectBuilder jsonObjectBuilder, String title) {
+		jsonObjectBuilder.field(
+			"title"
+		).stringValue(
+			title
+		);
+	}
+
+}
