11ce6f348d64f8f980b13762beef778df3bc5248
Alejandro Hernández <info@alejandrohdezma.com>
2018-10-02 09:49:23 +0200

    APIO-261 Add CORSFilter to Apio Sample

---
+++ apio-architect-sample/src/main/java/com/liferay/apio/architect/sample/internal/jaxrs/CORSFilter.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.sample.internal.jaxrs;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Enables Cross-Origin Resource Sharing (CORS) in the Apio Architect Sample.
+ *
+ * @author Alejandro Hernández
+ * @see    <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS">Cross-Origin Resource Sharing (CORS)</a>
+ * @review
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(liferay.apio.architect.application=true)",
+		"osgi.jaxrs.extension=true"
+	}
+)
+public class CORSFilter implements ContainerResponseFilter {
+
+	@Override
+	public void filter(
+		ContainerRequestContext requestContext,
+		ContainerResponseContext responseContext) {
+
+		MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+
+		headers.add("Access-Control-Allow-Origin", "*");
+		headers.add("Access-Control-Allow-Credentials", "true");
+		headers.add(
+			"Access-Control-Allow-Headers",
+			"origin, content-type, accept, authorization");
+		headers.add(
+			"Access-Control-Allow-Methods",
+			"GET, POST, PUT, DELETE, OPTIONS, HEAD");
+	}
+
+}
---
+++ apio-architect-sample/src/test/java/com/liferay/apio/architect/sample/internal/jaxrs/CORSFilterTest.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.sample.internal.jaxrs;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+/**
+ * @author Alejandro Hernández
+ */
+public class CORSFilterTest {
+
+	@Test
+	public void testFilterAddCORSHeaders() {
+		CORSFilter corsFilter = new CORSFilter();
+
+		ContainerResponseContext responseContext = Mockito.mock(
+			ContainerResponseContext.class);
+
+		MultivaluedMap<String, Object> headers = new MultivaluedHashMap<>();
+
+		Mockito.when(
+			responseContext.getHeaders()
+		).thenReturn(
+			headers
+		);
+
+		corsFilter.filter(null, responseContext);
+
+		assertThat(headers.getFirst("Access-Control-Allow-Origin"), is("*"));
+		assertThat(
+			headers.getFirst("Access-Control-Allow-Credentials"), is("true"));
+		assertThat(
+			headers.getFirst("Access-Control-Allow-Headers"),
+			is("origin, content-type, accept, authorization"));
+		assertThat(
+			headers.getFirst("Access-Control-Allow-Methods"),
+			is("GET, POST, PUT, DELETE, OPTIONS, HEAD"));
+	}
+
+}
