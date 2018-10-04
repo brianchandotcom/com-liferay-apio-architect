bafb15810183450ea991c9b9b76e6c7dde80b6bc
Victor Galan <victor.galan@liferay.com>
2018-10-04 15:44:31 +0200

    APIO-248 Add utility function to convert camelcase string to slugged strings. Ex. BlogPostings -> blog-postings

---
+++ apio-architect-impl/src/main/java/com/liferay/apio/architect/internal/annotation/representor/StringUtil.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.internal.annotation.representor;
+
+/**
+ * @author Alejandro Hernández
+ */
+public class StringUtil {
+
+	public static String toLowercaseSlug(String string) {
+		String sluggedString = String.join("-", string.split("(?=[A-Z])"));
+
+		return sluggedString.toLowerCase();
+	}
+
+}
