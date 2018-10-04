15b5c8a7819ed3e8967f02348737c977953e65aa
Victor Galan <victor.galan@liferay.com>
2018-10-04 15:35:38 +0200

    APIO-248 Add nested representor transformer, that transform a class annotated with Type to a nested representor.

---
+++ apio-architect-impl/src/main/java/com/liferay/apio/architect/internal/annotation/representor/NestedRepresentorTransformer.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.internal.annotation.representor;
+
+import static com.liferay.apio.architect.internal.annotation.representor.RepresentorTransformerUtil.addCommonFields;
+import static com.liferay.apio.architect.internal.annotation.representor.RepresentorTransformerUtil.getMethodFunction;
+
+import static org.apache.commons.lang3.reflect.MethodUtils.getMethodsListWithAnnotation;
+
+import com.liferay.apio.architect.annotation.Vocabulary.Field;
+import com.liferay.apio.architect.annotation.Vocabulary.RelatedCollection;
+import com.liferay.apio.architect.annotation.Vocabulary.Type;
+import com.liferay.apio.architect.representor.NestedRepresentor;
+
+import java.lang.reflect.Method;
+
+import java.util.List;
+
+/**
+ * @author Víctor Galán
+ */
+public class NestedRepresentorTransformer {
+
+	public static NestedRepresentor<?> toRepresentor(
+		Class<?> typeClass, NestedRepresentor.Builder<?> builder) {
+
+		Type type = typeClass.getAnnotation(Type.class);
+
+		NestedRepresentor.FirstStep<?> firstStep = builder.types(
+			type.value()
+		);
+
+		List<Method> methods = getMethodsListWithAnnotation(
+			typeClass, Field.class);
+
+		methods.forEach(method -> _processMethod(firstStep, method));
+
+		return firstStep.build();
+	}
+
+	private static void _processMethod(
+		NestedRepresentor.FirstStep<?> firstStep, Method method) {
+
+		Class<?> returnType = method.getReturnType();
+
+		Field field = method.getAnnotation(Field.class);
+
+		String key = field.value();
+
+		RelatedCollection relatedCollection = method.getAnnotation(
+			RelatedCollection.class);
+
+		if (relatedCollection != null) {
+			firstStep.addRelatedCollection(
+				key, relatedCollection.value(), getMethodFunction(method));
+		}
+
+		addCommonFields(firstStep, method, returnType, key);
+	}
+
+}
