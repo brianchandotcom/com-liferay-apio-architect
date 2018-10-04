2eb5e6ba372527de27a81ddb41547819a0df7741
Victor Galan <victor.galan@liferay.com>
2018-10-04 15:51:37 +0200

    APIO-248 Add manager that tracks all the ActionRouters services, extract its type and publish a representor for each one

---
+++ apio-architect-impl/src/main/java/com/liferay/apio/architect/internal/annotation/ActionRouterRepresentorManager.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.internal.annotation;
+
+import com.liferay.apio.architect.identifier.Identifier;
+import com.liferay.apio.architect.related.RelatedCollection;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * @author Víctor Galán
+ */
+public interface ActionRouterRepresentorManager {
+
+	public void computeRepresentors(
+		Function<Class<? extends Identifier<?>>, String> nameFunction,
+		Map<String, List<RelatedCollection<?, ?>>> relatedCollections);
+
+}
