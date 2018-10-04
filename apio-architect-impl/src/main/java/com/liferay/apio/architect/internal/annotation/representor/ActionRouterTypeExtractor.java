07b1e3495e5b52326ef9da4f30e87d9890ab329e
Victor Galan <victor.galan@liferay.com>
2018-10-04 15:33:40 +0200

    APIO-248 Add utility function to extract the type param of the ActionRouter

---
+++ apio-architect-impl/src/main/java/com/liferay/apio/architect/internal/annotation/representor/ActionRouterTypeExtractor.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.internal.annotation.representor;
+
+import com.liferay.apio.architect.annotation.Vocabulary.Type;
+import com.liferay.apio.architect.functional.Try;
+import com.liferay.apio.architect.identifier.Identifier;
+import com.liferay.apio.architect.internal.unsafe.Unsafe;
+import com.liferay.apio.architect.internal.wiring.osgi.util.GenericUtil;
+import com.liferay.apio.architect.router.ActionRouter;
+
+/**
+ * @author Alejandro Hernandez
+ * @author Víctor Galán
+ */
+public class ActionRouterTypeExtractor {
+
+	public static Try<Class<? extends Identifier>> extractTypeClass(
+		ActionRouter<?> actionRouter) {
+
+		Try<Class<Object>> classTry = GenericUtil.getGenericTypeArgumentTry(
+			actionRouter.getClass(), ActionRouter.class, 0);
+
+		return classTry.filter(
+			type -> type.isAnnotationPresent(Type.class)
+		).map(
+			Unsafe::unsafeCast
+		);
+	}
+
+}
