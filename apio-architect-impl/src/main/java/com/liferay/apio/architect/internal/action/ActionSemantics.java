632a66a1e1655f7ecacc7e5987541fa268b7ac20
Alejandro Hernández <info@alejandrohdezma.com>
2018-10-31 13:19:40 +0100

    APIO-244 Create ActionSemantics class
    This class is the result of joining ActionKey + Action classes. This class contains information about an action's name, method or resource along with its execute function, the parameters needed for it and its return type.

---
+++ apio-architect-impl/src/main/java/com/liferay/apio/architect/internal/action/ActionSemantics.java
@@ -0,0 +1,218 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.internal.action;
+
+import static com.liferay.apio.architect.internal.action.ImmutableActionSemantics.actionSemantics;
+
+import static java.util.Arrays.asList;
+
+import com.liferay.apio.architect.internal.action.resource.Resource;
+import com.liferay.apio.architect.operation.HTTPMethod;
+
+import io.vavr.CheckedFunction1;
+
+import java.util.List;
+
+import org.immutables.value.Value.Immutable;
+import org.immutables.value.Value.Parameter;
+import org.immutables.value.Value.Style;
+
+/**
+ * Instances of this class contains semantic information about an action like
+ *
+ * @author Alejandro Hernández
+ * @review
+ */
+@Immutable(builder = false)
+@Style(allParameters = true, of = "actionSemantics")
+public abstract class ActionSemantics {
+
+	/**
+	 * Starts creating a new {@link ActionSemantics} by providing information
+	 * about the action's resource.
+	 *
+	 * @review
+	 */
+	public static NameStep ofResource(Resource resource) {
+		return name -> method -> paramClasses -> returnClass ->
+			executeFunction -> () -> actionSemantics(
+				resource, name, method, asList(paramClasses), returnClass,
+				executeFunction);
+	}
+
+	/**
+	 * The function that executes the action.
+	 *
+	 * @review
+	 */
+	@Parameter(order = 5)
+	public abstract CheckedFunction1<List<?>, ?> executeFunction();
+
+	/**
+	 * The method in which the action is executed.
+	 *
+	 * @review
+	 */
+	@Parameter(order = 2)
+	public abstract String method();
+
+	/**
+	 * The action's name.
+	 *
+	 * @review
+	 */
+	@Parameter(order = 1)
+	public abstract String name();
+
+	/**
+	 * The list of param classes.
+	 *
+	 * @review
+	 */
+	@Parameter(order = 3)
+	public abstract List<Class<?>> paramClasses();
+
+	/**
+	 * The action's resource.
+	 *
+	 * @review
+	 */
+	@Parameter(order = 0)
+	public abstract Resource resource();
+
+	/**
+	 * The class returned by the action.
+	 *
+	 * @review
+	 */
+	@Parameter(order = 4)
+	public abstract Class<?> returnClass();
+
+	@FunctionalInterface
+	public interface BuildStep {
+
+		/**
+		 * Creates the {@link ActionSemantics} object with the information
+		 * provided to the builder.
+		 *
+		 * @review
+		 */
+		public ActionSemantics build();
+
+	}
+
+	@FunctionalInterface
+	public interface ExecuteStep {
+
+		/**
+		 * Provides information about the function action's execute function.
+		 * This function receives the list of params in the order provided in
+		 * the {@link ProvideMethodStep#receivesParams(Class[])} method.
+		 *
+		 * @review
+		 */
+		public BuildStep executeFunction(
+			CheckedFunction1<List<?>, ?> executeFunction);
+
+	}
+
+	@FunctionalInterface
+	public interface MethodStep {
+
+		/**
+		 * Provides information about the method in which the action is
+		 * executed.
+		 *
+		 * @review
+		 */
+		public default ProvideMethodStep method(HTTPMethod httpMethod) {
+			return method(httpMethod.name());
+		}
+
+		/**
+		 * Provides information about the method in which the action is
+		 * executed.
+		 *
+		 * @review
+		 */
+		public ProvideMethodStep method(String method);
+
+	}
+
+	@FunctionalInterface
+	public interface NameStep {
+
+		/**
+		 * Provides information about the the action's name.
+		 *
+		 * @review
+		 */
+		public MethodStep name(String name);
+
+	}
+
+	@FunctionalInterface
+	public interface ProvideMethodStep {
+
+		/**
+		 * Specifies that the action does not receive any params.
+		 *
+		 * @review
+		 */
+		public default ReturnStep receivesNoParams() {
+			return receivesParams();
+		}
+
+		/**
+		 * Provides information about the params needed by the action.
+		 *
+		 * <p>
+		 * The param instances will be provided in the {@link
+		 * #executeFunction()} in the same order as their classes in this
+		 * method. {@link Void} classes will be ignored (will be provided as
+		 * {@code null}. For the {@link
+		 * com.liferay.apio.architect.annotation.Id} or {@link
+		 * com.liferay.apio.architect.annotation.ParentId} params, the
+		 * annotation class should be provided to the list.
+		 * </p>
+		 *
+		 * @review
+		 */
+		public ReturnStep receivesParams(Class<?>... classes);
+
+	}
+
+	@FunctionalInterface
+	public interface ReturnStep {
+
+		/**
+		 * Provides information about the class returned by the action.
+		 *
+		 * @review
+		 */
+		public ExecuteStep returns(Class<?> returnClass);
+
+		/**
+		 * Specifies that the action does not return anything.
+		 *
+		 * @review
+		 */
+		public default ExecuteStep returnsNothing() {
+			return returns(Void.class);
+		}
+
+	}
+
+}
---
+++ apio-architect-impl/src/test/java/com/liferay/apio/architect/internal/action/ActionSemanticsTest.java
@@ -0,0 +1,167 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.internal.action;
+
+import static com.liferay.apio.architect.operation.HTTPMethod.GET;
+
+import static java.lang.String.join;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.hamcrest.core.Is.is;
+
+import com.liferay.apio.architect.internal.action.resource.Resource.Paged;
+
+import io.vavr.CheckedFunction1;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * @author Alejandro Hernández
+ */
+public class ActionSemanticsTest {
+
+	@Test
+	public void testBuilderCreatesActionSemantics() throws Throwable {
+		ActionSemantics actionSemantics = ActionSemantics.ofResource(
+			Paged.of("name")
+		).name(
+			"action"
+		).method(
+			GET
+		).receivesParams(
+			String.class, Long.class
+		).returns(
+			Long.class
+		).executeFunction(
+			_join
+		).build();
+
+		assertThat(actionSemantics.resource(), is(Paged.of("name")));
+		assertThat(actionSemantics.name(), is("action"));
+		assertThat(actionSemantics.method(), is("GET"));
+		assertThat(
+			actionSemantics.paramClasses(), contains(String.class, Long.class));
+		assertThat(actionSemantics.returnClass(), is(equalTo(Long.class)));
+
+		CheckedFunction1<List<?>, ?> executeFunction =
+			actionSemantics.executeFunction();
+
+		String result = (String)executeFunction.apply(Arrays.asList("1", "2"));
+
+		assertThat(result, is("1-2"));
+	}
+
+	@Test
+	public void testBuilderWithoutParamsCreatesActionSemantics()
+		throws Throwable {
+
+		ActionSemantics actionSemantics = ActionSemantics.ofResource(
+			Paged.of("name")
+		).name(
+			"action"
+		).method(
+			GET
+		).receivesNoParams(
+		).returns(
+			Long.class
+		).executeFunction(
+			_join
+		).build();
+
+		assertThat(actionSemantics.resource(), is(Paged.of("name")));
+		assertThat(actionSemantics.name(), is("action"));
+		assertThat(actionSemantics.method(), is("GET"));
+		assertThat(actionSemantics.paramClasses(), is(empty()));
+		assertThat(actionSemantics.returnClass(), is(equalTo(Long.class)));
+
+		CheckedFunction1<List<?>, ?> executeFunction =
+			actionSemantics.executeFunction();
+
+		String result = (String)executeFunction.apply(Arrays.asList("1", "2"));
+
+		assertThat(result, is("1-2"));
+	}
+
+	@Test
+	public void testBuilderWithoutReturnCreatesActionSemantics()
+		throws Throwable {
+
+		ActionSemantics actionSemantics = ActionSemantics.ofResource(
+			Paged.of("name")
+		).name(
+			"action"
+		).method(
+			GET
+		).receivesNoParams(
+		).returnsNothing(
+		).executeFunction(
+			_join
+		).build();
+
+		assertThat(actionSemantics.resource(), is(Paged.of("name")));
+		assertThat(actionSemantics.name(), is("action"));
+		assertThat(actionSemantics.method(), is("GET"));
+		assertThat(actionSemantics.paramClasses(), is(empty()));
+		assertThat(actionSemantics.returnClass(), is(equalTo(Void.class)));
+
+		CheckedFunction1<List<?>, ?> executeFunction =
+			actionSemantics.executeFunction();
+
+		String result = (String)executeFunction.apply(Arrays.asList("1", "2"));
+
+		assertThat(result, is("1-2"));
+	}
+
+	@Test
+	public void testBuilderWithStringMethodCreatesActionSemantics()
+		throws Throwable {
+
+		ActionSemantics actionSemantics = ActionSemantics.ofResource(
+			Paged.of("name")
+		).name(
+			"action"
+		).method(
+			"POST"
+		).receivesNoParams(
+		).returnsNothing(
+		).executeFunction(
+			_join
+		).build();
+
+		assertThat(actionSemantics.resource(), is(Paged.of("name")));
+		assertThat(actionSemantics.name(), is("action"));
+		assertThat(actionSemantics.method(), is("POST"));
+		assertThat(actionSemantics.paramClasses(), is(empty()));
+		assertThat(actionSemantics.returnClass(), is(equalTo(Void.class)));
+
+		CheckedFunction1<List<?>, ?> executeFunction =
+			actionSemantics.executeFunction();
+
+		String result = (String)executeFunction.apply(Arrays.asList("1", "2"));
+
+		assertThat(result, is("1-2"));
+	}
+
+	@SuppressWarnings("unchecked")
+	private static final CheckedFunction1<List<?>, Object> _join = list -> join(
+		"-", (List<String>)list);
+
+}
