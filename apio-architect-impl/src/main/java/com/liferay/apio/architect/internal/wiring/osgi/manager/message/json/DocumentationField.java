9eed695a980dff1dfc9e1c225b0b0b25615418a7
Victor Galan <victor.galan@liferay.com>
2018-11-26 14:46:39 +0100

    APIO-296 Create `DocumentationField` class that holds information about a fieldName and its type

---
+++ apio-architect-impl/src/main/java/com/liferay/apio/architect/internal/wiring/osgi/manager/message/json/DocumentationField.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.internal.wiring.osgi.manager.message.json;
+
+import java.util.Optional;
+
+/**
+ * @author Víctor Galán
+ */
+public class DocumentationField {
+
+	public static DocumentationField of(String name, FieldType fieldType) {
+		return of(name, fieldType, null);
+	}
+
+	public static DocumentationField of(
+		String name, FieldType fieldType, String extraType) {
+
+		return new DocumentationField(name, fieldType, extraType);
+	}
+
+	public Optional<String> getExtraType() {
+		return Optional.ofNullable(_extraType);
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public FieldType getType() {
+		return _type;
+	}
+
+	public enum FieldType {
+
+		BOOLEAN, BOOLEAN_LIST, DATE, DATE_LIST, FILE, LINKED_MODEL,
+		NESTED_MODEL, NESTED_MODEL_LIST, NUMBER, NUMBER_LIST,
+		RELATED_COLLECTION, STRING, STRING_LIST
+
+	}
+
+	private DocumentationField(String name, FieldType type, String extraType) {
+		_name = name;
+		_type = type;
+		_extraType = extraType;
+	}
+
+	private final String _extraType;
+	private final String _name;
+	private final FieldType _type;
+
+}
