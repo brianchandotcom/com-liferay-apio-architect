28052aa5b454c63d4d502b17e7e86e779ba840e9
Javier Gamarra <nhpatt@gmail.com>
2019-01-09 16:49:27 +0000

    APIO-249 sf

--- apio-architect-impl/src/main/java/com/liferay/apio/architect/internal/action/ActionSemantics.java
+++ apio-architect-impl/src/main/java/com/liferay/apio/architect/internal/action/ActionSemantics.java
@@ -19,7 +19,6 @@ import static java.util.stream.Collectors.toList;
 
 import com.liferay.apio.architect.annotation.Id;
 import com.liferay.apio.architect.form.Body;
-import com.liferay.apio.architect.form.Form;
 import com.liferay.apio.architect.internal.alias.ProvideFunction;
 import com.liferay.apio.architect.internal.annotation.Action;
 import com.liferay.apio.architect.operation.HTTPMethod;
@@ -34,13 +33,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 
 import javax.ws.rs.ForbiddenException;
-import javax.ws.rs.NotFoundException;
 
 /**
  * Instances of this class contains semantic information about an action like
@@ -124,21 +121,21 @@ public final class ActionSemantics {
 	}
 
 	/**
-	 * The list of permission classes.
-	 *
-	 * @review
-	 */
-	public List<Class<?>> getPermissionProvidedClasses() {
-		return _permissionProvidedClasses;
-	}
-
-	/**
 	 * The permission method that checks if we can execute an action.
 	 *
 	 * @review
 	 */
 	public CheckedFunction1<List<?>, Boolean> getPermissionMethod() {
 		return _permissionMethod;
+	}
+
+	/**
+	 * The list of permission classes.
+	 *
+	 * @review
+	 */
+	public List<Class<?>> getPermissionProvidedClasses() {
+		return _permissionProvidedClasses;
 	}
 
 	/**
@@ -387,13 +384,6 @@ public final class ActionSemantics {
 		}
 
 		@Override
-		public ExecuteStep permissionProvidedClasses(Class<?>... classes) {
-			_actionSemantics._permissionProvidedClasses = Arrays.asList(classes);
-
-			return this;
-		}
-
-		@Override
 		public ExecuteStep permissionMethod() {
 			_actionSemantics._permissionMethod = params -> true;
 
@@ -405,6 +395,14 @@ public final class ActionSemantics {
 			CheckedFunction1<List<?>, Boolean> permissionMethod) {
 
 			_actionSemantics._permissionMethod = permissionMethod;
+
+			return this;
+		}
+
+		@Override
+		public ExecuteStep permissionProvidedClasses(Class<?>... classes) {
+			_actionSemantics._permissionProvidedClasses = Arrays.asList(
+				classes);
 
 			return this;
 		}
@@ -643,8 +641,8 @@ public final class ActionSemantics {
 	private String _method;
 	private String _name;
 	private List<Class<?>> _paramClasses = new ArrayList<>();
-	private List<Class<?>> _permissionProvidedClasses = new ArrayList<>();
 	private CheckedFunction1<List<?>, Boolean> _permissionMethod;
+	private List<Class<?>> _permissionProvidedClasses = new ArrayList<>();
 	private Resource _resource;
 	private Class<?> _returnClass;
 
